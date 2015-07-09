583c9c26539a220a0b54ebf173ae79b2abdc3e62
skumar <skumar@agiletrailblazers.com>
2015-07-09 10:32:30 -0400

    edited run

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-  res.render('index', { title: 'Express', name: 'sasi' });
-=======
   res.render('index', { title: 'Express', name: 'Arti'});
->>>>>>> c9fd6de9ee8b14725b387b90d4370ba918735240
 });
-
-
 
 module.exports = router;
