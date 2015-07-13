e31556ac8ce1fcf405aa8ebe0e9e81d755b9f2e5
Kai Prout <kprout@agiletrailblazers.com>
2015-07-13 11:24:07 -0400

    Created contenful.js under api folder and added access token information for PREVIEW.

---
+++ API/contentful.js
@@ -0,0 +1,9 @@
+var contentful = require('contentful');
+
+var client = contentful.createClient({
+  accessToken: 'ff5d84851e19189914363e91468dd0c14f18a1f120ad42404e7fac9fc02216b4',
+  host: 'preview.contentful.com',
+  space: 'jzmztwi1xqvn'
+});
+
+// module.exports =
