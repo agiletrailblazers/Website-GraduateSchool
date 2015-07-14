b4274bedf32d75982ec6391f9bc9f67cb5972646
Kai Prout <kprout@agiletrailblazers.com>
2015-07-14 15:48:42 -0400

    Adding test for what's new page.

--- routes/param/getroutemocker.js
+++ routes/param/getroutemocker.js
@@ -1,5 +1,8 @@
 module.exports = {
   getHomepage: function getHomepage(req, res) {
     res.send('respond with the homepage URL');
+  },
+  getWhatsNew: function(req, res) {
+    res.send('respond with the whats new URL')
   }
 }
--- test/indexSpec.js
+++ test/indexSpec.js
@@ -7,14 +7,25 @@ var expect = chai.expect;
 
 describe("Routes", function() {
   describe("GET Homepage", function() {
-      it("test home page", function() {
-        var req,res,spy;
+    it("test home page", function() {
+      var req,res,spy;
 
-        req = res = {};
-        spy = res.send = sinon.spy();
+      req = res = {};
+      spy = res.send = sinon.spy();
 
-        getRoutemocker.getHomepage(req, res);
-        expect(spy.calledOnce).to.equal(true);
-      });
+      getRoutemocker.getHomepage(req, res);
+      expect(spy.calledOnce).to.equal(true);
+    });
+  });
+  describe("GET What's New Page", function() {
+    it("tests what's new page", function() {
+      var req,res,spy;
+
+      req = res = {};
+      spy = res.send = sinon.spy();
+
+      getRoutemocker.getWhatsNew(req, res);
+      expect(spy.calledOnce).to.equal(true);
+    });
   });
 });
