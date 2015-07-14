b1c0a3e4f6a9187be3217336c108e2c581453a75
Kai Prout <kprout@agiletrailblazers.com>
2015-07-14 16:54:55 -0400

    Adding test for what's new page. With psudo code.

--- test/testSpec.js
+++ test/testSpec.js
@@ -1,12 +1,19 @@
-var chai=require('chai');
-var expect=require('chai').expect;
+var chai = require('chai');
+var expect = require('chai').expect;
+var contentful = require('../API/contentful.js');
+var homepg = require('../API/tester.js');
 
-var homepg=require('../API/tester.js');
 
- describe ('homepage', function(){
+ describe ('homepage', function() {
    it('returns lowercase of a string', function(){
      var inputword='Welcome to the Graduate School Home Page';
      var outputword = homepg.landingPage(inputword);
      expect(outputword).to.equal('welcome to the graduate school home page');
    });
+ });
+ describe('whats-new', function() {
+   var goodStatus = "200"
+   contenful.getWhatsNew(function(response){
+    //  Expect api status to return 200 and not 404 or 500.
+   })
  });
