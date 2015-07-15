f0e27ccb8d84cb23811cb7ed141ddddced8b11de
Kai Prout <kprout@agiletrailblazers.com>
2015-07-15 12:12:06 -0400

    Refactoring tests.

--- test/testSpec.js
+++ test/testSpec.js
@@ -14,14 +14,15 @@ describe ('homepage', function() {
 });
 
 describe('/whats-new', function() {
+  var contentfulServer = nock('https://cdn.contentful.com')
+        .get('/spaces/jzmztwi1xqvn/entries/4QlvJ0GeQw4AY2QOq8SUMY?access_token=940e9e7a8f323bf2678b762426cc7349f2d0c339f6b6376a19e1b04e93c21652')
+        .reply(200, {
+          sidebarHeader: 'http://graduateschool.edu/images/whats_new_success.jpg',
+          sidebarTitle: 'LET US WORK WITH YOU TO ACHIEVE GREAT RESULTS. SEE WHAT SOME OF OUR CLIENTS HAVE TO SAY:',
+          statusCode: "200"
+        });
   it("should return a 200 response code", function(done) {
-    nock('https://cdn.contentful.com')
-      .get('/spaces/jzmztwi1xqvn/entries/4QlvJ0GeQw4AY2QOq8SUMY?access_token=940e9e7a8f323bf2678b762426cc7349f2d0c339f6b6376a19e1b04e93c21652')
-      .reply(200, {
-        sidebarHeader: 'http://graduateschool.edu/images/whats_new_success.jpg',
-        sidebarTitle: 'LET US WORK WITH YOU TO ACHIEVE GREAT RESULTS. SEE WHAT SOME OF OUR CLIENTS HAVE TO SAY:',
-        statusCode: "200"
-      });
+    contentfulServer;
     contentful.getWhatsNew(function(response){
       var goodStatus = 200;
       expect(response.statusCode).to.equal(goodStatus);
@@ -43,13 +44,7 @@ describe('/whats-new', function() {
     });
   });
   it("should return a JSON", function() {
-    nock('https://cdn.contentful.com')
-      .get('/spaces/jzmztwi1xqvn/entries/4QlvJ0GeQw4AY2QOq8SUMY?access_token=940e9e7a8f323bf2678b762426cc7349f2d0c339f6b6376a19e1b04e93c21652')
-      .reply(200, {
-        sidebarHeader: 'http://graduateschool.edu/images/whats_new_success.jpg',
-        sidebarTitle: 'LET US WORK WITH YOU TO ACHIEVE GREAT RESULTS. SEE WHAT SOME OF OUR CLIENTS HAVE TO SAY:',
-        statusCode: "200"
-      });
+    contentfulServer;
     contentful.getWhatsNew(function(response){
       expect(response).to.be.a('json');
     });
