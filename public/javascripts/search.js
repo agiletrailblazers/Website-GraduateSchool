368224e5b886284362543b4374b06fbc01e02223
Kai Prout <kprout@agiletrailblazers.com>
2015-07-21 15:13:58 -0400

    Adding validator for input length.

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -2,3 +2,16 @@ function performCourseSearch() {
   //TODO: error handling if no search criteria
   location.href = "course-search?search=" + document.getElementById("searchCriteria").value;
 }
+
+$(document).ready(function(){
+  $("#searchWarn").hide();
+  $("#course-search-button").click(function(e){
+    e.preventDefault();
+    var searchInput = $("#searchCriteria").val();
+    if (searchInput.length < 3) {
+      $("#searchWarn").show();
+    } else {
+      performCourseSearch();
+    }
+  });
+});
