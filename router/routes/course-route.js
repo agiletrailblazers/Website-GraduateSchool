eeb8af7154e42e4b3f6d51ba91d23eb80ee02ebd
Kai Prout <kaiprt@gmail.com>
2015-07-28 11:28:22 -0400

    Removing commented out code.

--- router/routes/course-route.js
+++ router/routes/course-route.js
@@ -13,7 +13,6 @@ router.get('/course-search', function(req, res, next){
     if (result && result.exactMatch) {
       //redirect to course details
       console.log("Exact course match found for " + result.courses[0].id + " - Redirecting.")
-      // res.redirect('course-detail?courseId=' + result.courses[0].id);
       res.redirect('courses/' + result.courses[0].id);
     }
     else {
