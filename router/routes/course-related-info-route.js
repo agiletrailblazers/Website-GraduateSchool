edd7e6d98d02443c4164b21e24ff9e5727e0a413
Kai Prout <kaiprt@gmail.com>
2015-08-07 12:10:30 -0400

    Initiating TASK - Node page change to open new links. Adding new routes file and including it in index.js.

---
+++ router/routes/course-related-info-route.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+// View Our Registration Policies
+// Ask A Question About This Course
+// Find Another Course
+// Bring this Course to Your Location
