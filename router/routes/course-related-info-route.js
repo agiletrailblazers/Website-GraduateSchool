5115fa8bcd2c1260fa2bdd8d47ea3f7fb4b207e2
Kai Prout <kaiprt@gmail.com>
2015-08-07 12:16:59 -0400

    Adding route for registration policy and view.

--- router/routes/course-related-info-route.js
+++ router/routes/course-related-info-route.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 // View Our Registration Policies
+router.get('/registration/policy', function(req, res, next) {
+  res.render('course-related-info/registration_policy');
+});
+
 // Ask A Question About This Course
 // Find Another Course
 // Bring this Course to Your Location
+
+module.exports = router;
