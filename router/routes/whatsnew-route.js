1019052767bbd04d20fc3afc7061536ff7066dea
skumar <skumar@agiletrailblazers.com>
2015-08-11 15:27:00 -0400

    Reverted code changes to be uniform with the master

--- router/routes/whatsnew-route.js
+++ router/routes/whatsnew-route.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var contentful = require("../../API/contentful.js");
+var async = require('async');
 var router = express.Router();
 
 // Get What's new page.
 router.get('/whats-new', function(req, res, next) {
   contentful.getWhatsNew(function(response) {
     res.render('whats_new', { entry: response.cmsEntry.fields });
-
   });
-  contentful.getAsset(function(response) {
-    res.render('whats_new', { entry1: response.asset.fields });
-
-  });
-
-
 });
 
 module.exports = router;
