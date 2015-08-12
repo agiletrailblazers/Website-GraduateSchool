941089ba496cc1164fa66528ae85c98130b92a0a
Kai Prout <kaiprt@gmail.com>
2015-08-12 15:24:41 -0400

    Adding route to get news entry on express side.

--- router/routes/news-route.js
+++ router/routes/news-route.js
@@ -1,2 +1,13 @@
 var express = require('express');
+var contentful = require('../../API/contentful.js');
+var async = require('async');
 var router = express.Router();
+
+router.get('/news/:news_id', function(req, res, next){
+  newsId = req.params.new_id;
+  contentful.getNewsDetail(function(response){
+    console.log("Made it here.");
+  }, newsId)
+});
+
+module.exports = router;
