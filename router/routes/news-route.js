1c1734bce27db902be3b12622a43d3ee68fae665
Eric Taylor <taylor_eric@yahoo.com>
2015-08-17 14:24:02 -0400

    Update main nav to pull from JSON from contentful

---
+++ router/routes/news-route.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var contentful = require('../../API/contentful.js');
+var async = require('async');
+var router = express.Router();
+
+router.get('/news/:news_slug', function(req, res, next){
+  slug = req.params.news_slug;
+  contentful.getNewsDetail(function(response){
+    console.log("Made it here: " + response);
+  }, slug)
+});
+
+module.exports = router;
