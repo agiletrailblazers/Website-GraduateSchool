d4e6400d400bd759ac9b9c3475fdf23fd3c574a0
Kai Prout <kaiprt@gmail.com>
2015-08-20 11:52:13 -0400

    Adding course outcomes to dynamically display on the page.

--- router/routes/course-route.js
+++ router/routes/course-route.js
@@ -85,7 +85,7 @@ router.get('/courses/:course_id', function(req, res, next){
 	    	courseDescription: striptags(content.class.description.formatted), courseCredit: content.class.credit,
 	    	courseLength: content.class.length.value, courseInterval: content.class.length.interval,
         courseObjective: content.class.objective, courseSchedule: content.class.schedule,
-        sessions: content.session, courseOutline: content.syllabus});
+        courseOutcomes: content.class.outcomes, sessions: content.session, courseOutline: content.syllabus});
     }
     else {
     	//handle error
