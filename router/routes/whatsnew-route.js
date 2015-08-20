6d1785cb26f7d925d5ee448199c5d422762f4b1d
Kai Prout <kaiprt@gmail.com>
2015-08-20 10:34:38 -0400

    Reindenting.

--- router/routes/whatsnew-route.js
+++ router/routes/whatsnew-route.js
@@ -6,40 +6,40 @@ var router = express.Router();
 // Get What's new page.
 router.get('/whats-new', function(req, res, next) {
   content = {};
-    var assetIdList="";
-    var spaceId = "jzmztwi1xqvn";
+  var assetIdList="";
+  var spaceId = "jzmztwi1xqvn";
   async.series([
     function(callback) {
       contentful.getWhatsNew(function(response) {
-          content.class = response;
-          content.class.cmsEntry.fields.topBanners.forEach(function(topBannersList){
-              assetIdList +=topBannersList.sys.id+",";
+        content.class = response;
+        content.class.cmsEntry.fields.topBanners.forEach(function(topBannersList){
+          assetIdList +=topBannersList.sys.id+",";
 
-            });
-         callback();
+        });
+        callback();
 
       });
     },
     function(callback) {
-        contentful.getContentAsset(spaceId,function (response, error, result) {
-            whatsnewheaderImageURLList="";
-            result.items.forEach(function(assetList){
-                if(assetIdList.indexOf(assetList.sys.id)>-1) {
-                    whatsnewheaderImageURLList +=assetList.fields.file.url+",";
-                 }
-            });
-            if (result != null) {
-                content.whatsnewheaderImageURLList=whatsnewheaderImageURLList;
-                callback();
-            } else {
-                content.whatsnewheaderImageURLList = '';
-                callback();
-            }
-            });
+      contentful.getContentAsset(spaceId,function (response, error, result) {
+        whatsnewheaderImageURLList="";
+        result.items.forEach(function(assetList){
+          if(assetIdList.indexOf(assetList.sys.id)>-1) {
+            whatsnewheaderImageURLList +=assetList.fields.file.url+",";
+          }
+        });
+        if (result != null) {
+          content.whatsnewheaderImageURLList=whatsnewheaderImageURLList;
+          callback();
+        } else {
+          content.whatsnewheaderImageURLList = '';
+          callback();
+        }
+      });
     },
   ], function(results) {
     res.render('whats_new', { entry: content.class.cmsEntry.fields,whatsnewheaderImageURLList: content.whatsnewheaderImageURLList});
-    });
+  });
 
 });
 
