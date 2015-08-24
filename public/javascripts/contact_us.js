ae8bb535f6f4a880c50e5c68e2010f82e7172958
Kai Prout <kaiprt@gmail.com>
2015-08-24 14:06:04 -0400

    Serializing data from form.

--- public/javascripts/contact_us.js
+++ public/javascripts/contact_us.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   $("#submitForm").click(function(e){
+    var data = $("contactForm").serialize();
+    console.log(data);
     e.preventDefault;
     $.ajax({
       url: "/mailer-contact-us",
       method: "POST",
-      data: "Hello World",
+      data: data
     }).success(function(response){
       console.log(response);
     }).fail(function(response){
