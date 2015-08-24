3a3726d5042584e77de0807609d1de1bd4db0d56
Kai Prout <kaiprt@gmail.com>
2015-08-24 12:16:02 -0400

    Adding new router file.

---
+++ router/routes/ajax-form-route.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var contentfulForms= require('../../API/contentful_forms.js');
+var async = require('async');
+var router = express.Router();
+
+router.post('/mailer-contact-us', function(req, res, next) {
+  
+});
+
+module.exports = router;
