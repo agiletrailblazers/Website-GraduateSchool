721fdbc1c9bff34544b1767ef82c5105682287dd
Kai Prout <kaiprt@gmail.com>
2015-08-24 12:20:56 -0400

    Adding bodyparser to route.

--- router/routes/ajax-form-route.js
+++ router/routes/ajax-form-route.js
@@ -2,9 +2,15 @@ var express = require('express');
 var contentfulForms= require('../../API/contentful_forms.js');
 var async = require('async');
 var router = express.Router();
+var bodyParser = require('body-parser');
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 router.post('/mailer-contact-us', function(req, res, next) {
-  
+
 });
 
 module.exports = router;
