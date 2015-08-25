6b3b163cc0b90f38de585d046e8231eb969606ab
Kai Prout <kaiprt@gmail.com>
2015-08-24 21:41:53 -0400

    Addding basic validations to test.

--- router/routes/ajax-form-route.js
+++ router/routes/ajax-form-route.js
@@ -4,11 +4,22 @@ var async = require('async');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var mailer = require('../../API/nodemailer.js');
+var validator = require('validator');
+
 
 router.post('/mailer-contact-us', function(req, res, next) {
   console.log("Body:", req.body);
   params = req.body;
-  // Validate params
+  // Validate params.firstName
+  validator.isLength(params.firstName, 3);
+  // Validate params.lastName
+  validator.isLength(params.lastName, 3);
+  // Validate params.email
+  validator.isEmail(params.email);
+  // Validate params.phone
+
+  // Validate params.comments
+
   // mailer.Mail.sendContactUs(params);
   res.send("You made it!");
 });
