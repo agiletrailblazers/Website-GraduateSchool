96dacdf5fff0f48ca7cf1825552a46b1bb3ec699
Kai Prout <kaiprt@gmail.com>
2015-08-25 11:52:35 -0400

    Fixing conditionals (they were opposite of the value I wanted).

--- router/routes/ajax-form-route.js
+++ router/routes/ajax-form-route.js
@@ -17,7 +17,7 @@ router.post('/mailer-contact-us', function(req, res, next) {
     case (!params.firstName):
       response.errors.firstName = "First name is empty.";
       break;
-    case (validator.isLength(params.firstName, 3)):
+    case (!validator.isLength(params.firstName, 3)):
       response.errors.firstName = "First name must be at least 3 characters.";
       break;
   }
@@ -26,7 +26,7 @@ router.post('/mailer-contact-us', function(req, res, next) {
     case (!params.lastName):
       response.errors.lastName = "Last name is empty.";
       break;
-    case (validator.isLength(params.lastName, 3)):
+    case (!validator.isLength(params.lastName, 3)):
       response.errors.lastName = "Last name must be at lease 3 characters.";
       break;
   }
@@ -35,7 +35,7 @@ router.post('/mailer-contact-us', function(req, res, next) {
     case (!params.email):
       response.errors.email = "Email is empty.";
       break;
-    case (validator.isEmail(params.email)):
+    case (!validator.isEmail(params.email)):
       response.errors.email = "Email is in the wrong format."
       break;
   }
