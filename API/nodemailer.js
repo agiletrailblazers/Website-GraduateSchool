5fffb586fc1604dbd30b5004511f9f43238c4faa
Kai Prout <kaiprt@gmail.com>
2015-08-24 21:12:35 -0400

    Stitching together mailer and ajax post route for CONTACT US (incomplete).

--- API/nodemailer.js
+++ API/nodemailer.js
@@ -4,27 +4,43 @@ var config = require('konphyg')(__dirname + "/../config");
 
 
 var transporter = nodemailer.createTransport(smtpTransport({
-    host: config("endpoint").defaultEmailServerName,
-    port: config("endpoint").defaultEmailServerPort,
-    auth: {
-        user: config("endpoint").defaultEmailUserName,
-        pass: config("endpoint").defaultEmailUserPassword
-    }
+  host: config("endpoint").defaultEmailServerName,
+  port: config("endpoint").defaultEmailServerPort,
+  auth: {
+    user: config("endpoint").defaultEmailUserName,
+    pass: config("endpoint").defaultEmailUserPassword
+  }
 }));
 
+// *** LEAVE FOR REFERENCE ***
+// var mailOptions = {
+//     from: config("endpoint").defaultEmailUserName,
+//     to:  config("endpoint").defaultEmailToUserName,
+//     subject: config("endpoint").defaultEmailSubject,
+//     text: config("endpoint").defaultEmailText
+// };
 
-var mailOptions = {
-    from: config("endpoint").defaultEmailUserName,
-    to:  config("endpoint").defaultEmailToUserName,
-    subject: config("endpoint").defaultEmailSubject,
-    text: config("endpoint").defaultEmailText
-};
+// transporter.sendMail(mailOptions, function(error, info){
+//     if(error){
+//         return console.log(error);
+//     }
+//     console.log('Message sent: ' + info.response);
+//
+// });
 
-
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        return console.log(error);
+var Mail = {
+  sendContactUs: function(params) {
+    var mailAttributes = {
+      from: "", // params.from goes here
+      to: config("endpoint").defaultEmailToUserName,
+      subject: "", // params.subject goes here
+      html: // HTML Goes here
     }
-    console.log('Message sent: ' + info.response);
-
-});
+    transporter.sendMail(mailAttributes, function(error, info) {
+      if (error) {
+        return console.log(error);
+      }
+      console.log('Message sent: ' + info.response);
+    });
+  }
+}
--- router/routes/ajax-form-route.js
+++ router/routes/ajax-form-route.js
@@ -3,9 +3,13 @@ var contentfulForms= require('../../API/contentful_forms.js');
 var async = require('async');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var mailer = require('../../API/nodemailer.js');
 
 router.post('/mailer-contact-us', function(req, res, next) {
   console.log("Body:", req.body);
+  params = req.body;
+  // Validate params
+  // mailer.Mail.sendContactUs(params);
   res.send("You made it!");
 });
 
