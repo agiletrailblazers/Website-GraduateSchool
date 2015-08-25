3019977e5c0f250fa262fdd988db3b4132142965
Kai Prout <kaiprt@gmail.com>
2015-08-25 15:35:22 -0400

    Stitching function to dynamically send data from params.

--- API/nodemailer.js
+++ API/nodemailer.js
@@ -1,6 +1,12 @@
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
 var config = require('konphyg')(__dirname + "/../config");
+// Email Templates
+var path = require('path');
+var EmailTemplate = require('email-templates').EmailTemplate;
+var templatesDir = path.resolve(__dirname, '..', 'templates');
+var template = new EmailTemplate(path.join(templatesDir, 'contactus-email'));
+// Transporter
 var transporter = nodemailer.createTransport(smtpTransport({
   host: config("endpoint").defaultEmailServerName,
   port: config("endpoint").defaultEmailServerPort,
@@ -29,17 +35,32 @@ module.exports = {
   // });
 
   sendContactUs: function(params) {
-    var mailAttributes = {
-      from: "", // params.from goes here
-      to: config("endpoint").defaultEmailToUserName,
-      subject: "", // params.subject goes here
-      html: "" // HTML Goes here
+    var locals = {
+      email: params.email,
+      name: {
+        first: params.firstName,
+        last: params.lastName
+      },
+      phone: params.phone,
+      comments: params.comments
     }
-    transporter.sendMail(mailAttributes, function(error, info) {
-      if (error) {
-        return console.log(error);
+    // Rendering template with locals.
+    template.render(locals, function(err, resulsts) {
+      if (err) {
+        return console.error(err)
       }
-      console.log('Message sent: ' + info.response);
+      var mailAttributes = {
+        from: config("endpoint").defaultEmailToUserName,
+        to: config("endpoint").defaultEmailToUserName,
+        subject: params.subject,
+        html: "" // HTML Goes here
+      };
+      transporter.sendMail(mailAttributes, function(error, info) {
+        if (error) {
+          return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+      });
     });
   }
 };
