3020d490e1b5dbb2768145c968830e4ddb3d1981
Rahul Eturu <rahul7155@gmail.com>
2015-08-27 15:17:51 -0400

    Added unit test case for form-route.js

---
+++ test/form-route-test.js
@@ -0,0 +1,49 @@
+var chai = require('chai');
+var expect = require('chai').expect;
+var contentful_forms = require("../API/contentful_forms.js");
+var nock = require('nock');
+var should = require("should");
+var test = require('tap').test;
+
+test('form route test case 1', function(t) {
+    var contentfulformServer = nock('https://cdn.contentful.com')
+        .get('/spaces/tz32dajhh9bn/entries/80IOLAFnVuYGk6U4ocooC/?access_token=093001a794ab16e4bf8ec4f7bc6740de4f267bc49549020ea3befbd5164754af')
+        .reply(200, {
+            'accept': 'application/json',
+        "fields": {
+                    "howDidYouHearAboutTraining": [
+                        {
+                            "name": "From a GS training officer."
+                        },
+                    ],
+        "title": "Training at Your Location"
+        }
+        });
+    contentfulformServer;
+    contentful_forms.getInquiryForm(function(response){
+        hearAboutTrainingString = 'From a GS training officer.';
+        console.log(response.fields.title);
+        expect(response.fields.howDidYouHearAboutTraining[0].name).to.equal(hearAboutTrainingString);
+        expect(response).to.be.a('object');
+    });
+    t.end();
+});
+
+test('form route test case 2', function(t) {
+    var contentfulformServer = nock('https://cdn.contentful.com')
+        .get('/spaces/tz32dajhh9bn/entries/6Av0MIjzZC2qIsGKUGyKS0?access_token=093001a794ab16e4bf8ec4f7bc6740de4f267bc49549020ea3befbd5164754af')
+        .reply(200, {
+            'accept': 'application/json',
+        "fields": {
+        "title": "Contact Us",
+        }
+        });
+    contentfulformServer;
+    contentful_forms.getContactUs(function(response){
+        fieldsTitle ="Contact Us";
+       expect(response.cmsEntry.fields.title).to.equal(fieldsTitle);
+        expect(response).to.be.a('object');
+    });
+    t.end();
+});
+
