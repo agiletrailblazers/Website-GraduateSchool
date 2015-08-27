13aa0e1a5faf83c765cf96cfa22230f8f75f8bd8
Kai Prout <kaiprt@gmail.com>
2015-08-27 14:45:47 -0400

    Implementing scroll to top on ajax failure.

--- public/javascripts/onsite-inquiry/onsite_inquiry.js
+++ public/javascripts/onsite-inquiry/onsite_inquiry.js
@@ -125,6 +125,9 @@ $(document).ready(function() {
             }
           }
           $("#alertError").slideDown();
+          $("html, body").animate({
+            scrollTop: 0
+          }, "slow");
         });
     }
   });
