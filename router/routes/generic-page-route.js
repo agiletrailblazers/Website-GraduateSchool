6d7c3453fe7f5c307025f3befb0e8daf4afc7951
Kai Prout <kaiprt@gmail.com>
2015-08-28 14:11:42 -0400

    Creating new router file.

---
+++ router/routes/generic-page-route.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var contentfulForms = require('../../API/contentful_forms.js');
+var contentful = require('../../API/contentful.js');
+var course = require('../../API/course.js');
+var async = require('async');
+var router = express.Router();
+
+
+
+module.exports = router;
