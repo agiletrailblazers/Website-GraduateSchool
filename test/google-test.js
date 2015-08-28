77bed541c29feb956d2f5e3c094806a0447511aa
Rahul Eturu <rahul7155@gmail.com>
2015-08-27 20:29:03 -0400

    Added testcase for googleRecaptcha

---
+++ test/google-test.js
@@ -0,0 +1,18 @@
+var chai = require('chai');
+var expect = require('chai').expect;
+var contentful_forms = require("../API/google.js");
+var nock = require('nock');
+var should = require("should");
+var test = require('tap').test;
+
+test('test for google capticha', function(t) {
+    var googleRecaptchaServer = nock('https://www.google.com')
+        .get('https://www.google.com/recaptcha/api/siteverify?secret=6Lfj4AsTAAAAAE0Bpvzcdxdg-dRvfAaS6ZI8_Duc&response=')
+        .reply(200, {
+        },'');
+    googleRecaptchaServer;
+    contentful_forms.verifyCaptcha(function(response){
+       expect(response.statusCode).to.eql(200);
+    },'');
+    t.end();
+});
