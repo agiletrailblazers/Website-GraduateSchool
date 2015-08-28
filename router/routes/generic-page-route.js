a813aa4952ae0c0e4c17b8abf8e9840d49211b5f
Kai Prout <kaiprt@gmail.com>
2015-08-28 15:09:36 -0400

    Removing required fields that aren't necessary.

--- router/routes/generic-page-route.js
+++ router/routes/generic-page-route.js
@@ -1,7 +1,5 @@
 var express = require('express');
-var contentfulForms = require('../../API/contentful_forms.js');
 var contentful = require('../../API/contentful.js');
-var course = require('../../API/course.js');
 var async = require('async');
 var router = express.Router();
 
