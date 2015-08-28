88cfb6fca8e124b2f48e35c4f6c37358836811a3
Eric Taylor <taylor_eric@yahoo.com>
2015-08-27 22:26:20 -0400

    Update test to an http post instead of a get; add failure case

--- test/google-test.js
+++ test/google-test.js
@@ -5,14 +5,27 @@ var nock = require('nock');
 var should = require("should");
 var test = require('tap').test;
 
-test('test for google capticha', function(t) {
-    var googleRecaptchaServer = nock('https://www.google.com',{allowUnmocked: true})
-        .get('https://www.google.com/recaptcha/api/siteverify?secret=6Lfj4AsTAAAAAE0Bpvzcdxdg-dRvfAaS6ZI8_Duc&response=')
+test('test for google captcha:success', function(t) {
+    var googleRecaptchaServer = nock('https://www.google.com')
+        .post('/recaptcha/api/siteverify?secret=6Lfj4AsTAAAAAE0Bpvzcdxdg-dRvfAaS6ZI8_Duc&response=')
         .reply(200, {
         },'');
     googleRecaptchaServer;
     contentful_forms.verifyCaptcha(function(response){
        expect(response.statusCode).to.eql(200);
+    },'');
+    t.end();
+});
+
+test('test for google captcha:error', function(t) {
+    var googleRecaptchaServer = nock('https://www.google.com')
+        .post('/recaptcha/api/siteverify?secret=6Lfj4AsTAAAAAE0Bpvzcdxdg-dRvfAaS6ZI8_Duc&response=')
+        .reply(500, {"success":false, "error-codes":["invalid-input-response"]
+        },'');
+    googleRecaptchaServer;
+    contentful_forms.verifyCaptcha(function(response){
+       expect(response.statusCode).to.eql(500);
+       expect(response.body).to.eq('{"success":false,"error-codes":["invalid-input-response"]}');
     },'');
     t.end();
 });
