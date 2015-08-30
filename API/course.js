1bf9c682aaa60591dc6784640d59ecb7f88dcefc
Eric Taylor <taylor_eric@yahoo.com>
2015-08-29 22:33:48 -0400

    Add file-based logger and convert all console logging

--- API/course.js
+++ API/course.js
@@ -1,20 +1,21 @@
 var request = require('request');
 var async = require('async');
 var config = require('konphyg')(__dirname + "/../config");
+var logger = require('../logger');
 
 module.exports = {
   performCourseSearch: function(callback, searchCriteria) {
     var courseApiUrl = config("endpoint").courseApiUrl;
-    console.log(courseApiUrl);
+    logger.debug(courseApiUrl);
     request({
       method: 'GET',
       url: courseApiUrl + '/api/courses?search=' + searchCriteria + ''
     }, function (error, response, body) {
       if (error != null || response == null || response.statusCode != 200) {
-        console.log("Exception occured performing course search. " + error);
+        logger.error("Exception occured performing course search. " + error);
         return callback(response, new Error("Exception occured performing couse search"), null);
       }
-      console.log('Status:', response.statusCode);
+      logger.debug('Status:', response.statusCode);
       result = JSON.parse(body);
       return callback(response, error, result);
     })
@@ -25,28 +26,26 @@ module.exports = {
       method: 'GET',
       url: courseApiUrl + '/api/courses/' + courseId + ''
     }, function (error, response, body) {
-        console.log(response.statusCode);
-      if (error != null || response == null || response.statusCode != 200) {
-        console.log("Exception occured performing exact course search. " + error);
+      logger.debug("Course Search: " + response.statusCode);
+      if (error != null || response.statusCode != 200) {
+        logger.error("Exception occured performing exact course search. " + error);
         return callback(response, new Error("Exception occured performing exact course search"), null);
       }
-      console.log('Status:', response.statusCode);
       result = JSON.parse(body);
       return callback(response, error, result);
     });
   },
   getSchedule: function(callback, courseId) {
-    console.log(courseId);
     var courseApiUrl = config("endpoint").courseApiUrl;
     request({
       method: 'GET',
       url: courseApiUrl + '/api/courses/' + courseId + '/sessions'
     }, function (error, response, body) {
-      if (error != null || response == null || response.statusCode != 200) {
-        console.log("Exception occured performing course schedule search. " + error);
+      logger.debug("Course Schedule: " + response.statusCode);
+      if (error != null || response.statusCode != 200) {
+        logger.error("Exception occured performing course schedule search. " + error);
         return callback(response, new Error("Exception occured performing couse search"), null);
       }
-      console.log('Status:', response.statusCode);
       result = JSON.parse(body);
       return callback(response, error, result);
     });
@@ -57,11 +56,11 @@ module.exports = {
       method: 'GET',
       url: courseApiUrl + '/api/courses'
     }, function (error, response, body) {
-      if (error != null || response == null || response.statusCode != 200) {
-        console.log("Exception occured getting all courses. " + error);
+      logger.debug("Get Courses: " + response.statusCode);
+      if (error != null || response.statusCode != 200) {
+        logger.error("Exception occured getting all courses. " + error);
         return callback(response, new Error("Exception occured getting all courses"), null);
       }
-      console.log('Status:', response.statusCode);
       result = JSON.parse(body).courses;
       return callback(response, error, result);
     });
@@ -73,11 +72,11 @@ module.exports = {
       method: 'GET',
       url: courseApiUrl + '/api/locations'
     }, function (error, response, body) {
+      logger.debug("Get Locations: " + response.statusCode);
       if (error != null || response == null || response.statusCode != 200) {
-        console.log("Exception occured getting all locations. " + error);
+        logger.error("Exception occured getting all locations. " + error);
         return callback(response, new Error("Exception occured getting all locations"), null);
       }
-      console.log('Status:', response.statusCode);
       result = JSON.parse(body);
       return callback(response, error, result);
     });
--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var contentful = require('./API/contentful.js');
 var config = require('konphyg')("./config");
+var logger = require('./logger');
 
 var app = express();
+
+logger.info('starting app');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,7 +18,6 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -64,6 +66,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
