aed7c388896ad9dd1e174866663df3db9bfa38bb
Boris Sekulic <bsekulic@agiletrailblazers.com>
2015-08-31 21:47:29 -0400

    adding whole bunch of UI stuff, structure, new pages, images, styles.

---
+++ public/javascripts/nav-top.js
@@ -0,0 +1,13 @@
+
+jQuery.noConflict()(function($){
+$(document).ready(function() {
+$('.toggle').click(function () {
+    $('.npanel').slideToggle('5000',"swing",
+        // Animation complete.
+         function(){
+	        $('.toggle').toggleClass('hide-panel');
+			      }
+		    );
+		});
+	});
+});
--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -3,16 +3,16 @@ function performCourseSearch() {
   location.href = "/course-search?search=" + document.getElementById("searchCriteria").value;
 }
 
-$(document).ready(function(){
-  $("#searchWarn").hide();
-  // Validating search on front-end.
-  $("#course-search-button").click(function(e){
-    e.preventDefault();
-    var searchInput = $("#searchCriteria").val();
-    if (searchInput.length < 3) {
-      $("#searchWarn").show();
-    } else {
-      performCourseSearch();
-    }
-  });
-});
+// $(document).ready(function(){
+//   $("#searchWarn").hide();
+//   // Validating search on front-end.
+//   $("#course-search-button").click(function(e){
+//     // e.preventDefault();
+//     var searchInput = $("#searchCriteria").val();
+//     if (searchInput.length < 3) {
+//       // $("#searchWarn").show();
+//     } else {
+//       performCourseSearch();
+//     }
+//   });
+// });
