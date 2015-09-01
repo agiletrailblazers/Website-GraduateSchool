6a4f55d42958f79b1666398225567ce7cf4ed9d7
Kai Prout <kaiprt@gmail.com>
2015-09-01 13:02:46 -0400

    Fixing jquery function that was breaking the multiselect courses.

--- public/javascripts/nav-top.js
+++ public/javascripts/nav-top.js
@@ -1,13 +1,10 @@
-
-jQuery.noConflict()(function($){
 $(document).ready(function() {
-$('.toggle').click(function () {
-    $('.npanel').slideToggle('5000',"swing",
-        // Animation complete.
-         function(){
-	        $('.toggle').toggleClass('hide-panel');
-			      }
-		    );
-		});
-	});
+  $('.toggle').click(function() {
+    $('.npanel').slideToggle('5000', "swing",
+      // Animation complete.
+      function() {
+        $('.toggle').toggleClass('hide-panel');
+      }
+    );
+  });
 });
