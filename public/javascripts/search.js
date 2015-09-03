035a3d273f6a9cfc5a3838340e6e911d5d4bf783
Boris Sekulic <bsekulic@agiletrailblazers.com>
2015-09-03 15:08:10 -0400

    adding title to search results page adding skeleton for refine results filters and results

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -3,7 +3,32 @@ function performCourseSearch() {
   location.href = "/course-search?search=" + document.getElementById("searchCriteria").value;
 }
 
-// $(document).ready(function(){
+var App = {
+  getSearchParamater: function() {
+    var url = window.location.href;
+    var regex = /=(.+)/;
+    var matches = url.match(regex);
+    if (matches) {
+      var match = matches[1];
+      $("#results-for").append(match);
+      }
+    }
+}
+
+
+$(document).ready(function(){
+    App.getSearchParamater();
+
+
+    $(window).bind('resize load', function() {
+        if ($(this).width() < 767) {
+            $('#refine-results').removeClass('in');
+            $('#refine-results').addClass('out');
+        } else {
+            $('#refine-results').removeClass('out');
+            $('#refine-results').addClass('in');
+        }
+    });
 //   $("#searchWarn").hide();
 //   // Validating search on front-end.
 //   $("#course-search-button").click(function(e){
@@ -15,4 +40,4 @@ function performCourseSearch() {
 //       performCourseSearch();
 //     }
 //   });
-// });
+});
