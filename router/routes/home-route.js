3db6efadf200a48c3ee74dfae5e1dcdc5f3d85d8
Boris Sekulic <bsekulic@agiletrailblazers.com>
2015-09-11 10:52:01 -0400

    adding backend functions/variables

--- router/routes/home-route.js
+++ router/routes/home-route.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var contentful = require('../../API/contentful.js');
 var router = express.Router();
-
+var async = require('async');
+var prune = require('underscore.string/prune');
+var logger = require('../../logger');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  contentful.getHomepageSlider(function(content) {
-    res.render('index', { title: 'Graduate School', name: 'Home Page', slider: content, homepage:true });
-  });
+  data = {};
+  async.parallel([
+    function(callback) {
+      contentful.getHomepageSlider(function(content) {
+        data.slider = content;
+        callback();
+      });
+    },
+    function(callback) {
+      contentful.getNewsRecent(function(response) {
+        data.news = response;
+        callback();
+     });
+   }
+ ], function(results) {
+   res.render('index', { title: 'Graduate School', name: 'Home Page', slider: data.slider, news: data.news, ​prune:prune, homepage:true });
+ });
 });
 
 module.exports = router;
