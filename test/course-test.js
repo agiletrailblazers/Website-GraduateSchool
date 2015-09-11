9005cd22a257eb148ba30943812f6219bc678061
Rahul Eturu <rahul7155@gmail.com>
2015-09-11 17:22:27 -0400

    TK-01866 Update Client-Side Code to support G2G Filter

--- test/course-test.js
+++ test/course-test.js
@@ -102,3 +102,32 @@ test('course-search with government search  criteria,numRequested and cityState'
   },params);
   t.end();
 });
+
+test('course-search with government search  criteria,numRequested,cityState and G2G', function(t) {
+    //use endpoing from config even for tests
+  var courseApiUrl = config("endpoint").courseApiUrl;
+  var params ={searchCriteria:"government",numRequested:"100",cityState:"Washington, DC",selectedG2G:"true"};
+    //test a 200 ok
+  var courseServer = nock(courseApiUrl)
+    .get('/api/courses?search=government&numRequested=100&filter=city_state:Washington,%20DC&filter=status:C')
+      .reply(200, {
+         "exactMatch": true,
+         "numRequested":"100",
+         "numFound":"235",
+         "courses": [
+           {
+             "courseId": "WRIT7043D",
+             "courseTitle": "Plain Writing: It is the Law (Classroom-Day)",
+             "courseDescription": "The Plain Writing Act of 2015 (October 13, 2010) requires the Federal government to..."
+           }
+           ]
+      });
+  courseServer;
+  course.performCourseSearch(function(response, error, result){
+     expect(response.statusCode).to.eql(200);
+     expect(result.numRequested).to.eql("100");
+     expect(result.numFound).to.eql("235");
+  },params);
+  t.end();
+});
+
