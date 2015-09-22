fe609c72a95d04263ae1256a3663e0862bea21ef
Kai Prout <kaiprt@gmail.com>
2015-09-22 11:05:44 -0400

    Creating initial geocoding function.

--- public/javascripts/course_detail.js
+++ public/javascripts/course_detail.js
@@ -1,27 +1,35 @@
-
-var map;
+var geocoder, map;
 mapApp = {
   start: function() {
     map = new google.maps.Map(document.getElementById('map-canvas'), {
       zoom: 8,
       center: {lat: 38.8859870, lng: -77.0212}
     });
+  },
+  codeAddress: function(address) {
+    geocoder.geocode( { 'address': address }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
   }
-} 
+}
 $(document).ready(function(){
   $('#map-canvas').hide();
   $('#close-map').hide();
   $('.glyphicon-map-marker').click(function(e) {
-    e.preventDefault();
-    $('#map-canvas').slideDown();
-    $('#close-map').show();
+    // e.preventDefault();
+    var address = $(this).data('address');
     mapApp.start();
     google.maps.event.addListenerOnce(map, 'idle', function() {
        google.maps.event.trigger(map, 'resize');
     });
-  });
-  $('#close-map').click(function(e) {
-    e.preventDefault();
-    $('#map-canvas').slideUp();
+    mapApp.codeAddress(address);
   });
 });
