900aa9dd2f39b487be904a7493f523dc8bcb5fc6
Kai Prout <kaiprt@gmail.com>
2015-09-23 11:06:07 -0400

    Commit due to branch change.'

--- public/javascripts/course_detail.js
+++ public/javascripts/course_detail.js
@@ -1,18 +1,24 @@
-var geocoder, map;
+var map;
+var geocoder;
 mapApp = {
   start: function() {
     map = new google.maps.Map(document.getElementById('map-canvas'), {
       zoom: 8,
-      center: {lat: 38.8859870, lng: -77.0212}
+      center: {
+        lat: 38.8859870,
+        lng: -77.0212
+      }
     });
   },
   codeAddress: function(address) {
-    geocoder.geocode( { 'address': address }, function(results, status) {
+    geocoder.geocode({
+      'address': address
+    }, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         map.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
+          map: map,
+          position: results[0].geometry.location
         });
       } else {
         alert("Geocode was not successful for the following reason: " + status);
@@ -20,16 +26,19 @@ mapApp = {
     });
   }
 }
-$(document).ready(function(){
+$(document).ready(function() {
   $('#map-canvas').hide();
   $('#close-map').hide();
   $('.glyphicon-map-marker').click(function(e) {
-    // e.preventDefault();
-    var address = $(this).data('address');
+    e.preventDefault();
     mapApp.start();
+    var address = $(this).data('address');
     google.maps.event.addListenerOnce(map, 'idle', function() {
-       google.maps.event.trigger(map, 'resize');
+      google.maps.event.trigger(map, 'resize');
     });
-    mapApp.codeAddress(address);
+    // mapApp.codeAddress(address);
+    $("#myModal").on("shown.bs.modal", function() {
+      google.maps.event.trigger(map, "resize");
+    });
   });
 });
