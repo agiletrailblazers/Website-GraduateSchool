cc471836fa6769827d471dd0ea48c447c281f72a
Kai Prout <kaiprt@gmail.com>
2015-09-24 11:15:35 -0400

    Adding function to display more or less for table data.

--- public/javascripts/course_detail.js
+++ public/javascripts/course_detail.js
@@ -22,7 +22,35 @@ mapApp = {
     });
   }
 }
+
+tableApp = {
+  limitTable: function() {
+    courseSessionLength = $("#courseSessionTable").data('totaltr');
+    x = 10;
+    $('.courseDetailSessionRow:lt(' + x + ')').show();
+    $('#loadMore').click(function() {
+      x = (x + 5 <= courseSessionLength) ? x + 5 : courseSessionLength;
+      $('.courseDetailSessionRow:lt(' + x + ')').show();
+      $('#showLess').show();
+      if (x == courseSessionLength) {
+        $('#loadMore').hide();
+      }
+    });
+    $('#showLess').click(function() {
+      x = (x - 5 < 0) ? 3 : x - 5;
+      $('.courseDetailSessionRow').not(':lt(' + x + ')').hide();
+      $('#loadMore').show();
+      $('#showLess').show();
+      if (x == 3) {
+        $('#showLess').hide();
+      }
+    });
+  }
+}
+
 $(document).ready(function() {
+  $(".courseDetailSessionRow").hide();
+  tableApp.limitTable();
   $('.glyphicon-map-marker').click(function(e) {
     e.preventDefault();
     var cityState = $(this).data('city');
