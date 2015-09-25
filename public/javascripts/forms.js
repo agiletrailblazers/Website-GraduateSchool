63ccf61e3b7a4cc60512d9b878381dfcfc0bc3fd
Rahul Eturu <rahul7155@gmail.com>
2015-09-25 07:53:25 -0400

    Filters for Forms to hide the section headers

--- public/javascripts/forms.js
+++ public/javascripts/forms.js
@@ -1,19 +1,30 @@
-$(document).ready(function() {
-  $('ul#filterForms li').click(function(e) {
+$(document).ready(function () {
+  $("ul#filterForms li").on("click", function (e) {
     e.preventDefault();
     name = $(this).attr('name').toLowerCase();
     if (name !== "all") {
       var assets = $("p.asset");
-      assets.each(function(index) {
-        contentTypeArray = $(this).data('contenttype').replace(/ /g,'').split(',');
+      assets.each(function (index, element) {
+        contentTypeArray = $(this).data('contenttype').replace(/ /g, '').split(',');
         if (jQuery.inArray(name, contentTypeArray) === -1) {
           $(this).hide();
         } else {
           $(this).show();
         }
+
       });
     } else {
       $("p.asset").show();
     }
+    headers = $(".assets").find("h3");
+    $(headers).each(function (i, e) {
+      if ($(this).siblings(":visible").length > 0) {
+        $(this).show();
+      }
+      else {
+        $(this).hide();
+      }
+    })
+
   });
 });
