4fe44ab529bf41e0dc715fd26b9c0420b189ea4a
Kai Prout <kaiprt@gmail.com>
2015-09-29 15:01:44 -0400

    Fixing accordion2 scroll top functionality.

--- public/javascripts/course_detail.js
+++ public/javascripts/course_detail.js
@@ -100,11 +100,15 @@ $(document).ready(function() {
   });
 
   $(".sessionExpand").eq(0).removeClass('collapsed');
-
-  $("[data-parent='#accordion2']").click(function(event) {
-    var thisId = $(this).attr("id");
-    $('html, body').animate({
-        scrollTop: $("#"+thisId+"").offset().top
-    }, 2000);
+  
+  $(function () {
+      $('#accordion2').on('shown.bs.collapse', function (e) {
+          var offset = $(this).find('.collapse.in').prev('.panel-heading');
+          if(offset) {
+              $('html,body').animate({
+                  scrollTop: $(offset).offset().top
+              }, 777);
+          }
+      });
   });
 });
