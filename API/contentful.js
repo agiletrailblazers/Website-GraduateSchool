8eb3073902de6783444e2f77ec593efd1fe08e4e
Eric Taylor <taylor_eric@yahoo.com>
2015-09-30 17:31:58 -0400

    B-01628 Added basics for alerts to show on homepage

--- API/contentful.js
+++ API/contentful.js
@@ -305,6 +305,19 @@ module.exports = {
       }
       return callback(testimonial);
     });
+  },
+  getAlerts: function(callback) {
+    request({
+      method: 'GET',
+      url: 'https://cdn.contentful.com/spaces/jzmztwi1xqvn/entries?content_type=22JH0IRqc0iEqM2uMgMyyy',
+      headers: {
+      'Authorization': 'Bearer 940e9e7a8f323bf2678b762426cc7349f2d0c339f6b6376a19e1b04e93c21652'
+       }
+    }, function(error, response, body) {
+      logger.debug("Get Alerts from Contentful: " + response.statusCode);
+      cmsEntry = JSON.parse(body);
+      return callback(cmsEntry.items);
+    });
   }
 
 };
