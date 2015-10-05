34dd5858837530163bf86655be8e05387d84b8cf
Kai Prout <kaiprt@gmail.com>
2015-10-05 15:41:46 -0400

    Adding error handling and displaying generic if link isn't selected.

--- router/routes/form-route.js
+++ router/routes/form-route.js
@@ -10,35 +10,35 @@ var logger = require('../../logger');
 router.get('/forms/onsite-inquiry', function(req, res, next) {
 	var fields, courses, locations, states;
 	async.parallel([
-        function(callback) {
-        	logger.debug('Get contentful fields');
-        	contentfulForms.getInquiryForm(function(response) {
-        		fields = response.fields;
-        		callback();
-        	});
-        },
-        function(callback) {
-        	logger.debug('Get list of all courses');
-        	course.getCourses(function(response, error, result) {
-        		courses = result;
-        		callback();
-        	});
-        },
-        function(callback) {
-        	logger.debug('Get list of all locations');
-        	course.getLocations(function(response, error, result) {
-        		locations = result;
-        		callback();
-        	});
-        },
-        function(callback) {
-        	logger.debug("Get us states");
-        	contentful.getReferenceData('us-states', function(result) {
-        		states = result;
-        		callback();
-        	});
-        }
-    ], function(results) {
+		function(callback) {
+			logger.debug('Get contentful fields');
+			contentfulForms.getInquiryForm(function(response) {
+				fields = response.fields;
+				callback();
+			});
+		},
+		function(callback) {
+			logger.debug('Get list of all courses');
+			course.getCourses(function(response, error, result) {
+				courses = result;
+				callback();
+			});
+		},
+		function(callback) {
+			logger.debug('Get list of all locations');
+			course.getLocations(function(response, error, result) {
+				locations = result;
+				callback();
+			});
+		},
+		function(callback) {
+			logger.debug("Get us states");
+			contentful.getReferenceData('us-states', function(result) {
+				states = result;
+				callback();
+			});
+		}
+	], function(results) {
 		res.render('forms/onsite_inquiry', {title: fields.title,
 			topParagraph: fields.topParagraph,
 			highlightedParagraph: fields.highlightedParagraph,
@@ -48,60 +48,65 @@ router.get('/forms/onsite-inquiry', function(req, res, next) {
 			courses: courses,
 			locations: locations,
 			states: states});
+		});
 	});
-});
 
-//Get Contact Us page.
-router.get('/forms/contact-us', function(req, res, next) {
-  var spaceId = "tz32dajhh9bn";
-  contentfulForms.getContactUs(function(response) {
-    	  logger.debug(response);
-    	  logger.debug("Subject Line:", response.cmsEntry.fields.subjectLine);
-          res.render('forms/contact_us', {title: response.cmsEntry.fields.title,
-        	  subjectLine: response.cmsEntry.fields.subjectLine,
-						topParagraph: response.cmsEntry.fields.topParagraph,
-						relatedLinks: response.cmsEntry.fields.relatedLinks
-        	  });
-        });
-    });
+	//Get Contact Us page.
+	router.get('/forms/contact-us', function(req, res, next) {
+		var spaceId = "tz32dajhh9bn";
+		contentfulForms.getContactUs(function(response) {
+			logger.debug(response);
+			logger.debug("Subject Line:", response.cmsEntry.fields.subjectLine);
+			res.render('forms/contact_us', {title: response.cmsEntry.fields.title,
+				subjectLine: response.cmsEntry.fields.subjectLine,
+				topParagraph: response.cmsEntry.fields.topParagraph,
+				relatedLinks: response.cmsEntry.fields.relatedLinks
+			});
+		});
+	});
 
-//Get Request duplicate Form Page
-router.get('/forms/request-duplicate-form', function (req, res, next) {
-  var fields, states, pageTitle;
-	var getQuery = new RegExp(/\?(.*)/);
-	var query = getQuery.exec(req.originalUrl);
-	if(query[1] === 'coursetype=cc') {
-		pageTitle = "Request Course Completion Certificate";
-	}
-	if (query[1] === 'coursetype=og') {
-		pageTitle = "Request Official Grade Report";
-	}
-	console.log(query);
-  async.parallel([
-    function (callback) {
-      logger.debug('Get contentful fields');
-      contentfulForms.getDuplicateForms(function (response) {
-        fields = response;
-        callback();
-      });
-    },
-    function (callback) {
-      logger.debug("Get us states");
-      contentful.getReferenceData('us-states', function (result) {
-        states = result;
-        callback();
-      });
-    }
-  ], function (results) {
-    res.render('forms/request_course_completion_certificate', {
-      sectionTitle: fields.sectionTitle,
-      sectionHeaderDescription: fields.sectionHeaderDescription,
-      sectionFooterDescription: fields.sectionFooterDescription,
-      title: pageTitle,
-			relatedLinks: fields.relatedLinks,
-      states: states
-    });
-  });
-});
+	//Get Request duplicate Form Page
+	router.get('/forms/request-duplicate-form', function (req, res, next) {
+		var fields, states, pageTitle;
+		var getQuery = new RegExp(/\?(.*)/);
+		var query = getQuery.exec(req.originalUrl);
+		if (query) {
+			if (query[1] === 'coursetype=cc') {
+				pageTitle = "Request Course Completion Certificate";
+			} else if(query[1] === 'coursetype=og') {
+				pageTitle = "Request Official Grade Report";
+			} else {
+				pageTitle = "Request Official Document";
+			}
+		} else {
+			pageTitle = "Request Official Document";
+		}
+		console.log(query);
+		async.parallel([
+			function (callback) {
+				logger.debug('Get contentful fields');
+				contentfulForms.getDuplicateForms(function (response) {
+					fields = response;
+					callback();
+				});
+			},
+			function (callback) {
+				logger.debug("Get us states");
+				contentful.getReferenceData('us-states', function (result) {
+					states = result;
+					callback();
+				});
+			}
+		], function (results) {
+			res.render('forms/request_course_completion_certificate', {
+				sectionTitle: fields.sectionTitle,
+				sectionHeaderDescription: fields.sectionHeaderDescription,
+				sectionFooterDescription: fields.sectionFooterDescription,
+				title: pageTitle,
+				relatedLinks: fields.relatedLinks,
+				states: states
+			});
+		});
+	});
 
-module.exports = router;
+	module.exports = router;
