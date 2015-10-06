402e686d773effe8f2005cc67cba189a4daa4b98
Eric Taylor <taylor_eric@yahoo.com>
2015-10-05 21:27:58 -0400

    Add code to toggle to tab when menu is clicked

--- public/javascripts/nav-top.js
+++ public/javascripts/nav-top.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-  $('.toggle').click(function() {
+  $('.nav-toggle').click(function() {
+    // If menu is closed, toggle to selected tab
+    if($('.npanel').css('display') === 'none') {
+      var tab = $(this).data('tab');
+      $("#" + tab).trigger('click');
+    }
     $('.npanel').slideToggle('5000', "swing",
       // Animation complete.
       function() {
