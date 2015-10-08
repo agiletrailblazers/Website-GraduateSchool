6785e51898095573f075de7efe09e8ad55ae7547
Kai Prout <kaiprt@gmail.com>
2015-10-08 14:19:54 -0400

    Adding validation for lack of inputs.

--- public/javascripts/forms/customer_feedback.js
+++ public/javascripts/forms/customer_feedback.js
@@ -1,15 +1,26 @@
+var Validate = {
+  inputs: function() {
+    $("#feedbackForm input").blur(function() {
+      if (!$(this).val()) {
+        $("#alertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>Please add content.</p>");
+      }
+    });
+  }
+}
+
 $(document).ready(function() {
   $("#submitForm").click(function(event) {
     event.preventDefault();
+    Validate.inputs();
     var dataForm = {};
     dataForm.firstName = $("#txtFirstName").val();
     dataForm.lastName = $("#txtLastName").val();
     dataForm.phone = $("#telPhone").val();
     dataForm.email = $("#email").val();
     dataForm.typePerson = $("input[name=typePerson]:checked", '#feedbackForm').val();
-    dataForm.feedbackCategories = $('input:checkbox:checked.feedbackCategories').map(function () {
-                                    return this.value;
-                                  }).get();
+    dataForm.feedbackCategories = $('input:checkbox:checked.feedbackCategories').map(function() {
+      return this.value;
+    }).get();
     dataForm.feedbackText = $("#txtFeedback").val();
   });
 });
