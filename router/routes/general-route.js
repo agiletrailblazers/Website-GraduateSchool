1e6dd7779fcc462adc72e22dc090d37e1d2eb5ff
Kai Prout <kaiprt@gmail.com>
2015-11-02 15:38:22 -0500

    Adding function to get all categories and associate them with questions. Rendering on front-end.

--- router/routes/general-route.js
+++ router/routes/general-route.js
@@ -6,19 +6,42 @@ var marked = require('marked');
 
 router.get(
   ['/development-training-frequently-asked-questions',
-    '/academic-program-frequently-asked-questions',
-    '/evening-weekend-frequently-asked-questions'
-  ],
-  function(req, res, next) {
-    if (req.url.indexOf("/development-training-frequently-asked-questions") > -1 ) {
-      title = "General Frequently Asked Questions (FAQ)"
-      faq = "general";
-    }
-    contentful.getFAQ(faq, function(response) {
-      faq = response;
-      faqEntries = faq.includes["Entry"];
-    });
-    res.render('misc/faq', {title: title, faqEntries: faqEntries, markdown: marked});
+  '/academic-program-frequently-asked-questions',
+  '/evening-weekend-frequently-asked-questions'
+],
+function(req, res, next) {
+  if (req.url.indexOf("/development-training-frequently-asked-questions") > -1 ) {
+    // TODO: Add logic to let front end know that this is under a url.
+    // TODO: Make conditionals or a switch for all possible FAQ's.
+  }
+  contentful.getFAQ(function(response) {
+    faq = response;
   });
+  faq.categories = [];
+  faq.items.forEach(function(item, i){
+    itemSlug = item.fields.slug.toString();
+    faq.categories[i] = {};
+    faq.categories[i].title = item.fields.title;
+    faq.categories[i].slug = itemSlug;
+  });
+  async.each(faq.categories, function(category, callback){
+    slug = category.slug;
+    contentful.getFAQCategory(slug, function(response) {
+      faq.categories[faq.categories.indexOf(category)].questions = [];
+      if (typeof response.includes != "undefined") {
+        faq.categories[faq.categories.indexOf(category)].questions= response.includes.Entry;
+      }
+    });
+  }, function(err){
+    if( err ) {
+      console.log('Error');
+    } else {
+      console.log('Success');
+    }
+});
+
+  // console.log(faq.categories);
+  res.render('misc/faq', {title: "Faq", faq: faq.categories, markdown: marked});
+});
 
 module.exports = router;
