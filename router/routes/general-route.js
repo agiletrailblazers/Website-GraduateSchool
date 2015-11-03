a9c5686f03f15f2af84bb38b1af57ccd3a9878b7
Kai Prout <kaiprt@gmail.com>
2015-11-03 09:15:57 -0500

    adding what I have.s

--- router/routes/general-route.js
+++ router/routes/general-route.js
@@ -6,42 +6,63 @@ var marked = require('marked');
 
 router.get(
   ['/development-training-frequently-asked-questions',
-  '/academic-program-frequently-asked-questions',
-  '/evening-weekend-frequently-asked-questions'
-],
-function(req, res, next) {
-  if (req.url.indexOf("/development-training-frequently-asked-questions") > -1 ) {
-    // TODO: Add logic to let front end know that this is under a url.
-    // TODO: Make conditionals or a switch for all possible FAQ's.
-  }
-  contentful.getFAQ(function(response) {
-    faq = response;
-  });
-  faq.categories = [];
-  faq.items.forEach(function(item, i){
-    itemSlug = item.fields.slug.toString();
-    faq.categories[i] = {};
-    faq.categories[i].title = item.fields.title;
-    faq.categories[i].slug = itemSlug;
-  });
-  async.each(faq.categories, function(category, callback){
-    slug = category.slug;
-    contentful.getFAQCategory(slug, function(response) {
-      faq.categories[faq.categories.indexOf(category)].questions = [];
-      if (typeof response.includes != "undefined") {
-        faq.categories[faq.categories.indexOf(category)].questions= response.includes.Entry;
-      }
-    });
-  }, function(err){
-    if( err ) {
-      console.log('Error');
-    } else {
-      console.log('Success');
+    '/academic-program-frequently-asked-questions',
+    '/evening-weekend-frequently-asked-questions'
+  ],
+  function(req, res, next) {
+    if (req.url.indexOf("/development-training-frequently-asked-questions") > -1) {
+      // TODO: Add logic to let front end know that this is under a url.
+      // TODO: Make conditionals or a switch for all possible FAQ's.
     }
-});
+    faq = {};
+    async.waterfall([
+      function(callback) {
+        contentful.getFAQ(function(response) {
+          faq.response = response;
+          // console.log("First function faq: ", faq);
+          callback(null, faq);
+        });
+      },
+      function(faq, callback) {
+        faq.categories = [];
+        faq.response.items.forEach(function(item, i) {
+          itemSlug = item.fields.slug.toString();
+          faq.categories[i] = {};
+          faq.categories[i].title = item.fields.title;
+          faq.categories[i].slug = itemSlug;
+          // console.log("Second function faq: ", faq);
+          callback(null, faq);
+        });
+      },
+      function(faq, callback) {
+        async.each(faq.categories, function(category, callback) {
+          // console.log(faq.categories);
+          // console.log(faq.categories.indexOf(category));
+          slug = category.slug;
+          contentful.getFAQCategory(slug, function(response) {
+            faq.categories[faq.categories.indexOf(category)].questions = [];
+            if (typeof response.includes != "undefined") {
+              faq.categories[faq.categories.indexOf(category)].questions = response.includes.Entry;
+            }
+          });
+          console.log(faq);
+          callback(null, faq);
+        }, function(err, result) {
+          if (err) {
+            console.log('Error');
+          } else {
+            console.log('Success');
+            console.log("Categories", result);
+            res.render('misc/faq', {
+              title: "FAQ",
+              faq: result,
+              markdown: marked
+            });
+          }
+        });
+      }
+    ]);
 
-  // console.log(faq.categories);
-  res.render('misc/faq', {title: "Faq", faq: faq.categories, markdown: marked});
-});
+  });
 
 module.exports = router;
