d76bc5b79ac35ee287928ec04987e961b69cd4b9
Rahul Eturu <rahul7155@gmail.com>
2015-11-05 21:51:00 -0500

    Added the catalog ejs changes

--- router/routes/catalog-route.js
+++ router/routes/catalog-route.js
@@ -12,7 +12,7 @@
           content = {};
           content.catlogTitle=  cmsEntryAsset.fields.catlogTitle;
           content.catalogFilter = cmsEntryAsset.fields.catalogFilter;
-            console.log(  content.catlogTitle);
+        console.log(  content.catlogTitle);
           arrayofAssetObj =[];
           if(typeof(cmsEntryAsset.fields.catlogFileAssets) !='undefined') {
             cmsEntryAsset.fields.catlogFileAssets.forEach(function (sectionFile) {
@@ -20,8 +20,10 @@
                 if (asset.sys.id == sectionFile.sys.id) {
                   assetObj = {};
                   assetObj.title = asset.fields.title;
+                  console.log(assetObj.title);
                   assetObj.description = asset.fields.description;
                   assetObj.url = asset.fields.file.url;
+                  assetObj.displayInSecondColumn = false;
                   arrayofAssetObj.push(assetObj);
                 }
               });
@@ -29,6 +31,19 @@
           }
           content.assetList=arrayofAssetObj;
           arrayOfContent.push(content);
+
+        });
+        arrayOfContent.forEach(function(categoryGroup) {
+          categoryGroup.assetList.sort(function(a,b) {
+             return a.title.localeCompare(b.title);
+          });
+          if (categoryGroup.assetList.length > 0) {
+            categoryGroup.assetList.forEach(function(asset,num) {
+            if (num > 8)  {
+              asset.displayInSecondColumn = true;
+           }
+         });
+          }
         });
         callback();
       });
