7f04182879c190050864f7412933de8a675984d7
Paul Friedman <paul@friedmanonline.net>
2015-11-19 17:49:03 -0500

    Update validation alert messages per updated decisions

--- public/javascripts/forms/formValidation.js
+++ public/javascripts/forms/formValidation.js
@@ -3,7 +3,7 @@ var length = function (id, alertId, fieldName, length) {
   var idForError = id.substring(1);
   var noNumbersPattern = new RegExp(/^[^0-9]+$/);
   if (input.trim().length < length) {
-    $(alertId).append("<p id='" + idForError + "-length'><span  class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>" + fieldName + "</strong> must be at least " + length + " characters.</p>");
+    $(alertId).append("<p id='" + idForError + "-length'><span  class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>" + fieldName + "</strong> is required and must be at least " + length + " characters.</p>");
   }
 }
 
@@ -37,7 +37,7 @@ var validate = {
     var idForError = id.substring(1);
     var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
     if (!pattern.test(email)) {
-        $(alertId).append("<p id='" + idForError + "-format'><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>" + fieldName + "</strong>  is incorrect.</p>");
+        $(alertId).append("<p id='" + idForError + "-format'><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>" + fieldName + "</strong> is required and must be a properly formatted email address.</p>");
     }
   }
 }
