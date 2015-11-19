a4c219884493de42779223d4711bb7f340ec2f74
Yuvaraj Ilangovan <yilangovan@agiletrailblazers.com>
2015-11-19 13:52:34 -0500

    added validation code changes for Mailsubscriptionform

--- public/javascripts/forms/subscription.js
+++ public/javascripts/forms/subscription.js
@@ -1,52 +1,4 @@
 var subscriptionFormValidate = {
-  testIdForLenghtAndNoNumbers: function (id, alertId, fieldName) {
-    var input = $(id).val();
-    var noNumbersPattern = new RegExp(/^[^0-9]+$/);
-    if (input.trim().length < 1) {
-      $(alertId).append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>" + fieldName + "</strong> is empty.</p>");
-    }
-    else if (!noNumbersPattern.test(input)) {
-      $(alertId).append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>" + fieldName + "</strong> should not have numbers.</p>");
-    }
-  },
-
-  firstName: function() {
-    var input = $("#txtFirstName").val();
-    var noNumbersPattern = new RegExp(/^[^0-9]+$/);
-    if (!noNumbersPattern.test(input)) {
-      $("#alertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>First Name</strong> should not have numbers.</p>");
-    }
-    if ((input.trim()).length < 3) {
-      $("#alertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>First Name</strong> should be at least 3 characters.</p>");
-    }
-  },
-  middleName: function() {
-    var input = $("#txtMiddleName").val();
-    var noNumbersPattern = new RegExp(/^[^0-9]+$/);
-    if (!noNumbersPattern.test(input)) {
-      $("#alertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>Last Name</strong> should not have numbers.</p>");
-    }
-    if ((input.trim()).length < 3) {
-      $("#alertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>Last Name</strong> should be at least 3 characters.</p>");
-    }
-  },
-  lastName: function() {
-    var input = $("#txtLastName").val();
-    var noNumbersPattern = new RegExp(/^[^0-9]+$/);
-    if (!noNumbersPattern.test(input)) {
-      $("#alertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>Last Name</strong> should not have numbers.</p>");
-    }
-    if ((input.trim()).length < 3) {
-      $("#alertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>Last Name</strong> should be at least 3 characters.</p>");
-    }
-  },
-  email: function() {
-    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-    var email = $("#txtEmail").val();
-    if (!pattern.test(email)) {
-      $("#alertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> Email address is incorrect.</p>");
-    }
-  },
   phone: function() {
     var pattern = new RegExp(/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/);
     var phone = $("#txtPhone").val();
@@ -64,12 +16,6 @@ var subscriptionFormValidate = {
       $("#alertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> Please choose a type of subscription.</p>");
     }
   },
-  city: function() {
-    var input = $("#txtCity").val();
-    if (input.trim().length < 3) {
-      $("#alertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>City</strong> is incorrect.</p>");
-    }
-  },
   state: function() {
     var input = $("#txtState").val();
     if (!input) {
@@ -81,12 +27,6 @@ var subscriptionFormValidate = {
     // TODO: Regex to see if it is only numbers.
     if (input.trim().length < 5) {
       $("#alertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>Zip code</strong> is incorrect.</p>");
-    }
-  },
-  streetAddress: function() {
-    var input = $("#txtStreet").val();
-    if (input.trim().length < 3) {
-      $("#alertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> <strong>Street address</strong> must be at least 3 characters.</p>");
     }
   },
   captcha: function(){
@@ -102,23 +42,22 @@ var _runSubscriptionFormValidation = function() {
   $("#alertError p").remove();
 
   subscriptionFormValidate.subscriptionType()
-  subscriptionFormValidate.firstName();
-  // subscriptionFormValidate.middleName();
-  // subscriptionFormValidate.testIdForLenghtAndNoNumbers("#txtFirstName", "#alertError", "First Name");
-  // subscriptionFormValidate.testIdForLenghtAndNoNumbers("#txtMiddleName", "#alertError", "Middle Name");
-  // subscriptionFormValidate.testIdForLenghtAndNoNumbers("#txtLastName", "#alertError", "Last Name");
-  subscriptionFormValidate.lastName();
+  validate.name("#txtFirstName", "#alertError", "First Name");
+  validate.name("#txtLastName", "#alertError", "Last Name");
 
   if($("#subscriptionTypeEmail").is(':checked')) {
-    subscriptionFormValidate.email();
+    validate.email("#txtEmail","#alertError", "Email");
   }
 
   if($("#subscriptionTypeMail").is(':checked')) {
-    subscriptionFormValidate.streetAddress();
-    subscriptionFormValidate.city();
+    validate.CheckEmptyString("#txtStreet","#alertError", "Street/P.O.Box");
+    validate.CheckEmptyString("#txtCity","#alertError", "City");
+    // subscriptionFormValidate.city();
     subscriptionFormValidate.state();
-    subscriptionFormValidate.zip();
-    subscriptionFormValidate.phone();
+    //subscriptionFormValidate.zip();
+    validate.CheckEmptyString("#txtZip","#alertError", "Zip code");
+    validate.CheckEmptyString("#txtPhone","#alertError", "Phone number");
+    //subscriptionFormValidate.phone();
   }
 
   subscriptionFormValidate.captcha();
