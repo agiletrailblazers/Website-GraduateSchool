453c181e1b4b33467842b6c9326aeb9d4efc0320
Rahul Eturu <rahul7155@gmail.com>
2015-12-17 14:33:09 -0500

    Added the fields for cutomerback feed back form

--- public/javascripts/forms/customer_feedback.js
+++ public/javascripts/forms/customer_feedback.js
@@ -7,10 +7,29 @@ var Validate = {
 
     });
   },
+   typePerson: function() {
+    if (!$(".typePerson").is(':checked')) {
+      $("#customerFeedbackFormAlertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>Please select Which Best Describes You?</p>");
+    }
+  },
+  feedbackCategories: function() {
+    if (!$(".feedbackCategories").is(':checked')) {
+      $("#customerFeedbackFormAlertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>Please select or enter Area Of Feedback.</p>");
+    }
+  },
   email: function(email) {
     var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    if ((email.trim().length == 0)) {
+      $("#customerFeedbackFormAlertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> Please enter Email.</p>");
+    }
     if ((email.trim().length !== 0) && (!pattern.test(email))) {
       $("#customerFeedbackFormAlertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> Email address is incorrect.</p>");
+    }
+  },
+  comments: function() {
+    var input = $("#txtFeedback").val();
+    if (input == '' || input == null || typeof(input) == 'undefined' || input.trim().length < 3) {
+      $("#customerFeedbackFormAlertError").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> Please enter your Feedback.</p>");
     }
   },
   captcha: function () {
@@ -25,6 +44,9 @@ var _runValidation = function () {
   $("#customerFeedbackFormAlertError p").remove();
   Validate.captcha();
   Validate.inputs();
+  Validate.typePerson();
+  Validate.feedbackCategories();
+  Validate.comments();
   Validate.email($("#txtCustomerEmail").val());
   if ($("#customerFeedbackFormAlertError p").length) {
     $("#customerFeedbackFormAlertError").slideDown("slow");
