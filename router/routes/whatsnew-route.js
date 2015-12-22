94824e3a856766a74ab16c5156f927095affc590
Yuvaraj Ilangovan <yilangovan@agiletrailblazers.com>
2015-12-22 15:11:09 -0500

    Added error handlers for Whatsnew-route page

--- router/routes/whatsnew-route.js
+++ router/routes/whatsnew-route.js
@@ -2,6 +2,7 @@ var express = require('express');
 var contentful = require("../../API/contentful.js");
 var async = require('async');
 var router = express.Router();
+var logger = require('../../logger');
 
 // Get What's new page.
 router.get('/whats-new', function(req, res, next) {
@@ -10,39 +11,53 @@ router.get('/whats-new', function(req, res, next) {
   var spaceId = "jzmztwi1xqvn";
   async.series([
     function(callback) {
-      contentful.getWhatsNew(function(response) {
-        content.class = response;
-        if (response && response.cmsEntry && response.cmsEntry.fields && response.cmsEntry.fields.topBanners){
-          content.class.cmsEntry.fields.topBanners.forEach(function(topBannersList) {
-          assetIdList += topBannersList.sys.id + ",";
-          });
+      contentful.getWhatsNew(function(response, error) {
+        if(error){
+          logger.error(error);
+          logger.error('Could not retrieve Whatsnew from Contentful. Redirecting to error page');
+          res.redirect('/error');
         }
-        callback();
+        else{
+          content.class = response;
+          if (response && response.cmsEntry && response.cmsEntry.fields && response.cmsEntry.fields.topBanners){
+            content.class.cmsEntry.fields.topBanners.forEach(function(topBannersList) {
+            assetIdList += topBannersList.sys.id + ",";
+            });
+          }
+          callback();
+        }
+
       });
     },
     function(callback) {
       contentful.getContentAsset(spaceId, function(response, error, result) {
-        whatsnewheaderImageURLList = "";
-
-        if (result && result.items) {
-          result.items.forEach(function(assetList) {
-            if (assetIdList.indexOf(assetList.sys.id) > -1) {
-              whatsnewheaderImageURLList += assetList.fields.file.url + ",";
-            }
-          });
+        if(error){
+          logger.error(error);
+          logger.error('Could not retrieve ContentAsset from Contentful. Redirecting to error page');
+          res.redirect('/error');
         }
-        content.whatsnewheaderImageURLList = whatsnewheaderImageURLList;
-        callback();
+        else{
+          whatsnewheaderImageURLList = "";
+          if (result && result.items) {
+            result.items.forEach(function(assetList) {
+              if (assetIdList.indexOf(assetList.sys.id) > -1) {
+                whatsnewheaderImageURLList += assetList.fields.file.url + ",";
+              }
+            });
+          }
+          content.whatsnewheaderImageURLList = whatsnewheaderImageURLList;
+          callback();
+        }
       });
     },
   ], function(results) {
-    res.render('whats_new', {
-      title: 'What\'s new',
-      entry: content.class.cmsEntry.fields,
-      whatsnewheaderImageURLList: content.whatsnewheaderImageURLList
-    });
+      logger.error('why the hell its triggering');
+      res.render('whats_new', {
+        title: 'What\'s new',
+        entry: content.class.cmsEntry.fields,
+        whatsnewheaderImageURLList: content.whatsnewheaderImageURLList
+      });
   });
-
 });
 
 module.exports = router;
