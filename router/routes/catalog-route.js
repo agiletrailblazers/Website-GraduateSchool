01f398266d7f2abba0468c560c5d276a4b2cf3a5
Yuvaraj Ilangovan <yilangovan@agiletrailblazers.com>
2015-12-23 16:42:16 -0500

    added exception handlers in respective route API calls

--- router/routes/catalog-route.js
+++ router/routes/catalog-route.js
@@ -1,66 +1,89 @@
-  var express = require('express');
+var express = require('express');
   var contentful = require("../../API/contentful.js");
   var async = require('async');
   var router = express.Router();
+  var logger = require('../../logger');
+
   // Get Catalog Request Form  page.
   router.get('/catalog-request-form', function(req, res, next) {
   arrayOfContent=[];
   var catalogHardCopy = {};
   async.parallel([
     function(callback) {
-      contentful.getCatalogType(function(response) {
-        response.cmsEntry.forEach(function(cmsEntryAsset) {
-          content = {};
-          content.catlogTitle=  cmsEntryAsset.fields.catlogTitle;
-          content.catalogFilter = cmsEntryAsset.fields.catalogFilter;
-          content.catalogDisplayOrder = cmsEntryAsset.fields.catlogFilterOrder;
-          arrayofAssetObj =[];
-          if(typeof(cmsEntryAsset.fields.catlogFileAssets) !='undefined') {
-            cmsEntryAsset.fields.catlogFileAssets.forEach(function (sectionFile) {
-              response.cmsAsset.forEach(function (asset) {
-                if (asset.sys.id == sectionFile.sys.id) {
-                  assetObj = {};
-                  assetObj.title = asset.fields.title;
-                  assetObj.description = asset.fields.description;
-                  assetObj.url = asset.fields.file.url;
-                  assetObj.displayInSecondColumn = false;
-                  arrayofAssetObj.push(assetObj);
-                }
+      contentful.getCatalogType(function(response, error) {
+        if(error){
+          logger.error(error);
+          logger.error('Could not retrieve CatalogType from Contentful. Redirecting to error page');
+          res.redirect('/error');
+        }
+        else{
+          response.cmsEntry.forEach(function(cmsEntryAsset) {
+            content = {};
+            content.catlogTitle=  cmsEntryAsset.fields.catlogTitle;
+            content.catalogFilter = cmsEntryAsset.fields.catalogFilter;
+            content.catalogDisplayOrder = cmsEntryAsset.fields.catlogFilterOrder;
+            arrayofAssetObj =[];
+            if(typeof(cmsEntryAsset.fields.catlogFileAssets) !='undefined') {
+              cmsEntryAsset.fields.catlogFileAssets.forEach(function (sectionFile) {
+                response.cmsAsset.forEach(function (asset) {
+                  if (asset.sys.id == sectionFile.sys.id) {
+                    assetObj = {};
+                    assetObj.title = asset.fields.title;
+                    assetObj.description = asset.fields.description;
+                    assetObj.url = asset.fields.file.url;
+                    assetObj.displayInSecondColumn = false;
+                    arrayofAssetObj.push(assetObj);
+                  }
+                });
               });
-            });
-          }
-          content.assetList=arrayofAssetObj;
-          arrayOfContent.push(content);
-        });
-        arrayOfContent.sort(function(a, b) {
-          return (a.catalogDisplayOrder) - (b.catalogDisplayOrder);
-        });
-        arrayOfContent.forEach(function(categoryGroup) {
-          categoryGroup.assetList.sort(function(a,b) {
-             return a.title.localeCompare(b.title);
+            }
+            content.assetList=arrayofAssetObj;
+            arrayOfContent.push(content);
           });
-          if (categoryGroup.assetList.length > 16) {
-            categoryGroup.assetList.forEach(function(asset,num) {
-            if (num > categoryGroup.assetList.length/2)  {
-              asset.displayInSecondColumn = true;
-           }
-         });
-          }
-        });
-        callback();
+          arrayOfContent.sort(function(a, b) {
+            return (a.catalogDisplayOrder) - (b.catalogDisplayOrder);
+          });
+          arrayOfContent.forEach(function(categoryGroup) {
+            categoryGroup.assetList.sort(function(a,b) {
+               return a.title.localeCompare(b.title);
+            });
+            if (categoryGroup.assetList.length > 16) {
+              categoryGroup.assetList.forEach(function(asset,num) {
+              if (num > categoryGroup.assetList.length/2)  {
+                asset.displayInSecondColumn = true;
+             }
+           });
+            }
+          });
+          callback();
+        }
       });
     },function(callback) {
-      contentful.getCatalogRequestHardCopy(function(response) {
-        catalogHardCopy = response.cmsEntry;
-        catalogHardCopy.sort(function(a,b) {
-          return a.fields.catalogHardCopy.localeCompare(b.fields.catalogHardCopy);
-        });
-        callback();
+      contentful.getCatalogRequestHardCopy(function(response, error) {
+        if(error){
+          logger.error(error);
+          logger.error('Could not get CatalogRequestHardCopy from Contentful. Redirecting to error page');
+          res.redirect('/error');
+        }
+        else{
+          catalogHardCopy = response.cmsEntry;
+          catalogHardCopy.sort(function(a,b) {
+            return a.fields.catalogHardCopy.localeCompare(b.fields.catalogHardCopy);
+          });
+          callback();
+        }
       });
     },function(callback) {
-      contentful.getReferenceData('us-states', function(result) {
-        states = result;
-        callback();
+      contentful.getReferenceData('us-states', function(result, error) {
+        if(error){
+          logger.error(error);
+          logger.error('Could not get state details from Contentful. Redirecting to error page');
+          res.redirect('/error');
+        }
+        else{
+          states = result;
+          callback();
+        }
       });
     } ], function(results) {
       res.render('catalogs', {
--- router/routes/subscription-route.js
+++ router/routes/subscription-route.js
@@ -1,47 +1,67 @@
-  var express = require('express');
-  var contentful = require("../../API/contentful.js");
-  var async = require('async');
-  var router = express.Router();
-  var config = require('konphyg')(__dirname + '/../../config');
-  var contentfulForms = require('../../API/contentful_forms.js');
+var express = require('express');
+ var contentful = require("../../API/contentful.js");
+ var async = require('async');
+ var router = express.Router();
+ var config = require('konphyg')(__dirname + '/../../config');
+ var contentfulForms = require('../../API/contentful_forms.js');
+ var logger = require('../../logger');
 
-  // Get Catalog Request Form  page.
-  router.get('/subscription-form', function(req, res, next) {
-    arrayOfContent=[];
-    var catalogHardCopy = {};
-    async.parallel([
-      function(callback) {
-        contentful.getCatalogType(function(response) {
-          response.cmsEntry.forEach(function(cmsEntryAsset) {
-            content = {};
-            content.areaOfInterest=  cmsEntryAsset.fields.catlogTitle;
-            content.areaOfInterestDisplayOrder = cmsEntryAsset.fields.catlogFilterOrder;
-            arrayOfContent.push(content);
-          });
-          arrayOfContent.sort(function(a, b) {
-            return (a.catalogDisplayOrder) - (b.catalogDisplayOrder);
-          });
-          callback();
-        });
-      },
-      function(callback) {
-        var entryId = "2wAaVPf3aIWCAu0SeeI44O";
-        contentfulForms.getFormWithHeaderAndFooter(entryId, function(response, error) {
-          title = response.fields.sectionTitle;
-          callback();
-        });
-      },
-      function(callback) {
-        contentful.getReferenceData('us-states', function(result) {
-          states = result;
-          callback();
-        });
-      }], function(results) {
-        res.render('forms/subscription_form', {
-          entry : arrayOfContent, title: title,
-          states : states,
-          skipReCaptcha : config("properties").skipReCaptchaVerification
-        });
-    });
-  });
-  module.exports = router;
+ // Get subscription-form page.
+ router.get('/subscription-form', function(req, res, next) {
+   arrayOfContent=[];
+   var catalogHardCopy = {};
+   async.parallel([
+     function(callback) {
+       contentful.getCatalogType(function(response, error) {
+         if(error){
+           logger.warn(error);
+           logger.warn('Ignoring error retrieving the catalog types in subscription-form, because areaofinterest is not a mandatory field');
+         }
+         else{
+           response.cmsEntry.forEach(function(cmsEntryAsset) {
+             content = {};
+             content.areaOfInterest=  cmsEntryAsset.fields.catlogTitle;
+             content.areaOfInterestDisplayOrder = cmsEntryAsset.fields.catlogFilterOrder;
+             arrayOfContent.push(content);
+           });
+           arrayOfContent.sort(function(a, b) {
+             return (a.catalogDisplayOrder) - (b.catalogDisplayOrder);
+           });
+         }
+         callback();
+       });
+     },
+     function(callback) {
+       var entryId = "2wAaVPf3aIWCAu0SeeI44O";
+       contentfulForms.getFormWithHeaderAndFooter(entryId, function(response, error) {
+         if(error){
+           logger.warn(error);
+           logger.warn('Ignoring error retrieving the SectionTitle in subscription-form');
+         }
+         else{
+           title = response.fields.sectionTitle;
+         }
+         callback();
+       });
+     },
+     function(callback) {
+       contentful.getReferenceData('us-states', function(result, error) {
+         if(error){
+           logger.error(error);
+           logger.error('Could not get states from Contentful.getReferenceData API call. Redirecting to error page');
+           res.redirect('/error');
+         }
+         else{
+           states = result;
+           callback();
+         }
+       });
+     }], function(results) {
+       res.render('forms/subscription_form', {
+         entry : arrayOfContent, title: title,
+         states : states,
+         skipReCaptcha : config("properties").skipReCaptchaVerification
+       });
+   });
+ });
+ module.exports = router;
