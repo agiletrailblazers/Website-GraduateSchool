49821a6a306970fc486edba7924d4c6892645ba6
rahulkumareturu <rahulkumareturu@rahulkumareturus-MacBook-Pro.local>
2016-02-12 11:02:25 -0500

    Added the changes to show the landing page only in test,dev and local

--- router/routes/landing-page-route.js
+++ router/routes/landing-page-route.js
@@ -1,12 +1,8 @@
 var express = require('express');
-var contentful = require('../../API/contentful.js');
 var router = express.Router();
-var logger = require('../../logger');
-var marked = require('marked');
-var common = require("../../helpers/common.js");
 var config = require('konphyg')(__dirname + '/../../config');
 
-router.get(['/landing/:content_slug'], function(req, res, next) {
+router.get(['/landing-form'], function(req, res, next) {
     res.render('landing/landing_detail', {
       title: "landing",
       skipReCaptcha : config("properties").skipReCaptchaVerification
