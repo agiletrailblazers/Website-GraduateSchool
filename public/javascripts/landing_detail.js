c21eede6425e3af0a1ddc4eba11db2504367df40
Boris Sekulic <bsekulic@agiletrailblazers.com>
2016-03-02 15:33:12 -0500

    more refactoring, form styling fixes

--- public/javascripts/landing_detail.js
+++ public/javascripts/landing_detail.js
@@ -74,4 +74,12 @@ $(document).ready(function() {
     'speed' : 1000,
     'color' : 'black',
   });
+
+  //Controls plus-minus sign on top nav on mobile
+  $('.collapse').on('shown.bs.collapse', function() {
+    $(this).parent().find(".glyphicon-plus-sign").removeClass("glyphicon-plus-sign").addClass("glyphicon-minus-sign");
+  }).on('hidden.bs.collapse', function() {
+    $(this).parent().find(".glyphicon-minus-sign").removeClass("glyphicon-minus-sign").addClass("glyphicon-plus-sign");
+  });
+
 });
