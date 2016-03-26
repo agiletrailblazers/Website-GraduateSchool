8c08c97c5bab297e7fcc371c6f57541f20919b38
Ifti Khan <ikhan@agiletrailblazers.com>
2016-03-26 00:18:00 -0400

    GS-991. Clean up unit test.

--- test/gtog-test.js
+++ test/gtog-test.js
@@ -7,6 +7,66 @@ var should = require("should");
 var test = require('tap').test;
 var config = require('konphyg')(__dirname + "/../config");
 var common = require("../helpers/common.js")
+var proxyquire = require('proxyquire').noCallThru();
+
+var sessions = [
+  {
+    "classNumber": "00086407",
+    "segment": null,
+    "startDate": "2017-05-01",
+    "endDate": "2017-05-02",
+    "startTime": "08:30",
+    "endTime": "16:00",
+    "days": "Mon-Tue",
+    "scheduleMaximum": 2400,
+    "scheduleAvailable": 70,
+    "scheduleMinimum": 8,
+    "status": "C",
+    "notes": null,
+    "tuition": 1000,
+    "location": {
+      "id": "01",
+      "name": "National Arboretum",
+      "telephone": "2022454521",
+      "address1": "Stadium Amory Metro Stop",
+      "address2": "24th & R Streets NE",
+      "city": "Washington",
+      "state": "DC",
+      "postalCode": "20002"
+    },
+    "instructor": null,
+    "offeringSessionId": "class000000000091170",
+    "courseId": "cours000000000001652",
+    "courseCode": "PROJ8295D",
+    "courseTitle": "Making Sense of CC Errors 101",
+    "curricumTitle": "Program and Management Analysis"
+  }
+]
+
+var gTogResponse = {
+  "sys": {
+    "id": "60o2cWEaVaQAqwo2IcmcOU",
+    "type": "Entry",
+    "locale": "en-US"
+  },
+  "fields": {
+    "featureImage": {
+      "sys": {
+        "type": "Link",
+        "linkType": "Asset",
+        "id": "7JVkMp8CQMyW0Q0IokEaiu"
+      }
+    },
+    "section": "Graduate School USA is committed to confirming __Guaranteed-to-Go__ course sessions for our federal customers, making it easier for them to schedule and register for training through the fiscal year. We want you to be confident in planning your training expenditures.\n\n<a href=\"/search?search=&selectedG2G=true\" class=\"btn btn-primary btn-lg active\" role=\"button\">Search Guaranteed-to-Go Sessions</a>",
+    "relatedLinks": [
+      {
+        "url": "/search",
+        "name": "Search All Courses"
+      }
+    ],
+    "title": "Guaranteed-to-Go Classes"
+  }
+}
 
 test('test for getting G2G content', function(t) {
   var contentfulServer = nock('https://cdn.contentful.com', {
@@ -14,30 +74,7 @@ test('test for getting G2G content', function(t) {
       'Authorization': config("properties").spaces.content.authorization
     }
     }).get('/spaces/'+config("properties").spaces.content.spaceId+'/entries/60o2cWEaVaQAqwo2IcmcOU')
-    .reply(200, {
-      "sys": {
-        "id": "60o2cWEaVaQAqwo2IcmcOU",
-        "type": "Entry",
-        "locale": "en-US"
-      },
-      "fields": {
-        "featureImage": {
-          "sys": {
-            "type": "Link",
-            "linkType": "Asset",
-            "id": "7JVkMp8CQMyW0Q0IokEaiu"
-          }
-        },
-        "section": "Graduate School USA is committed to confirming __Guaranteed-to-Go__ course sessions for our federal customers, making it easier for them to schedule and register for training through the fiscal year. We want you to be confident in planning your training expenditures.\n\n<a href=\"/search?search=&selectedG2G=true\" class=\"btn btn-primary btn-lg active\" role=\"button\">Search Guaranteed-to-Go Sessions</a>",
-        "relatedLinks": [
-          {
-            "url": "/search",
-            "name": "Search All Courses"
-          }
-        ],
-        "title": "Guaranteed-to-Go Classes"
-      }
-  });
+    .reply(200, gTogResponse);
 
   contentful.getGtoGPage(function(response){
     expect(response.fields.title).to.equal("Guaranteed-to-Go Classes");
@@ -52,47 +89,13 @@ test('test for getting G2G curriculum Daytime', function(t) {
       'Authorization': authToken
     }
   }).get('/api/courses/sessions?status=c&sessiondomain=CD')
-    .reply(200, [
-      {
-        "classNumber": "00086407",
-        "segment": null,
-        "startDate": "2017-05-01",
-        "endDate": "2017-05-02",
-        "startTime": "08:30",
-        "endTime": "16:00",
-        "days": "Mon-Tue",
-        "scheduleMaximum": 2400,
-        "scheduleAvailable": 70,
-        "scheduleMinimum": 8,
-        "status": "C",
-        "notes": null,
-        "tuition": 1000,
-        "location": {
-          "id": "01",
-          "name": "National Arboretum",
-          "telephone": "2022454521",
-          "address1": "Stadium Amory Metro Stop",
-          "address2": "24th & R Streets NE",
-          "city": "Washington",
-          "state": "DC",
-          "postalCode": "20002"
-        },
-        "instructor": null,
-        "offeringSessionId": "class000000000091170",
-        "courseId": "cours000000000001652",
-        "courseCode": "PROJ8295D",
-        "courseTitle": "Making Sense of CC Errors 101",
-        "curricumTitle": "Program and Management Analysis"
-      }
-    ]
-  );
+    .reply(200, sessions);
 
   courseAPI.getSessions(function(error, response){
     expect(response[0].courseTitle).to.equal("Making Sense of CC Errors 101");
   }, authToken, 'c', 'CD');
   t.end();
 });
-
 
 test('test for getting G2G curriculum', function(t) {
   var authToken = "token123456789";
@@ -101,40 +104,7 @@ test('test for getting G2G curriculum', function(t) {
       'Authorization': authToken
     }
   }).get('/api/courses/sessions?status=c')
-    .reply(200, [
-      {
-        "classNumber": "00086407",
-        "segment": null,
-        "startDate": "2017-05-01",
-        "endDate": "2017-05-02",
-        "startTime": "08:30",
-        "endTime": "16:00",
-        "days": "Mon-Tue",
-        "scheduleMaximum": 2400,
-        "scheduleAvailable": 70,
-        "scheduleMinimum": 8,
-        "status": "C",
-        "notes": null,
-        "tuition": 1000,
-        "location": {
-          "id": "01",
-          "name": "National Arboretum",
-          "telephone": "2022454521",
-          "address1": "Stadium Amory Metro Stop",
-          "address2": "24th & R Streets NE",
-          "city": "Washington",
-          "state": "DC",
-          "postalCode": "20002"
-        },
-        "instructor": null,
-        "offeringSessionId": "class000000000091170",
-        "courseId": "cours000000000001652",
-        "courseCode": "PROJ8295D",
-        "courseTitle": "Making Sense of CC Errors 101",
-        "curricumTitle": "Program and Management Analysis"
-      }
-    ]
-  );
+    .reply(200, sessions);
 
   courseAPI.getSessions(function(error, response){
     expect(response[0].courseTitle).to.equal("Making Sense of CC Errors 101");
@@ -149,47 +119,13 @@ test('test for getting sessions', function(t) {
       'Authorization': authToken
     }
   }).get('/api/courses/sessions')
-    .reply(200, [
-      {
-        "classNumber": "00086407",
-        "segment": null,
-        "startDate": "2017-05-01",
-        "endDate": "2017-05-02",
-        "startTime": "08:30",
-        "endTime": "16:00",
-        "days": "Mon-Tue",
-        "scheduleMaximum": 2400,
-        "scheduleAvailable": 70,
-        "scheduleMinimum": 8,
-        "status": "C",
-        "notes": null,
-        "tuition": 1000,
-        "location": {
-          "id": "01",
-          "name": "National Arboretum",
-          "telephone": "2022454521",
-          "address1": "Stadium Amory Metro Stop",
-          "address2": "24th & R Streets NE",
-          "city": "Washington",
-          "state": "DC",
-          "postalCode": "20002"
-        },
-        "instructor": null,
-        "offeringSessionId": "class000000000091170",
-        "courseId": "cours000000000001652",
-        "courseCode": "PROJ8295D",
-        "courseTitle": "Making Sense of CC Errors 101",
-        "curricumTitle": "Program and Management Analysis"
-      }
-    ]
-  );
+    .reply(200, sessions);
 
   courseAPI.getSessions(function(error, response){
     expect(response[0].courseTitle).to.equal("Making Sense of CC Errors 101");
   }, authToken);
   t.end();
 });
-
 
 test('test for getting sessions error', function(t) {
   var authToken = "token123456789";
@@ -242,3 +178,37 @@ test('test for getting sessions error', function(t) {
   }, authToken);
   t.end();
 });
+
+// test('displayG2GPage', function(t) {
+//
+//   var res = {};
+//
+//   var req = {};
+//
+//   // mock out our collaborators (i.e. the required libraries) so that we can verify behavior of our controller
+//   var controller = proxyquire('../router/routes/gtog-controller.js',
+//   {
+//
+//     "../../API/course.js": {
+//       getSessions: function (cb, authToken, sessionStatus, sessionDomain) {
+//         cb(null, sessions);
+//       }
+//     },
+//     "../../../API/contentful.js": {
+//       getGtoGPage: function(cb) {
+//         cb(gTogResponse);
+//       }
+//     }
+//    });
+//
+//   res.render = function(page, arguments) {
+//       expect(page).to.eql('gtog/gtog');
+//       expect(arguments.title).to.eql(gTogResponse.title);
+//       expect(arguments.content).to.eql(course);
+//       expect(arguments.curriculumSessions).to.eql(courseSession);
+//   };
+//
+//   controller.displayG2GPage(req, res, null);
+//
+//   t.end();
+// });
