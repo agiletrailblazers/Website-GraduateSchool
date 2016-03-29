2d5698f270cba06595299bbee99d4836ada011fd
Ifti Khan <ikhan@agiletrailblazers.com>
2016-03-29 15:17:06 +0100

    GS-1015. Changes based on code review.

--- router/routes/course-route.js
+++ router/routes/course-route.js
@@ -146,17 +146,19 @@ router.get('/courses/:course_id_or_code', function(req, res, next){
           courseData.session.forEach(function(session) {
             var epRegDeadline = momentTz.tz(session.origStartDate, 'YYYY MM DD', "America/New_York").add(14, 'days').add(18, 'hours');
             // if the deadline has gone then status is set to 'C'
-            if (epRegDeadline.isBefore(momentTz().tz("America/New_York"))) {
+            var currTime = momentTz().tz("America/New_York");
+            if ( epRegDeadline.isBefore(currTime) ||
+                 epRegDeadline.isSame(currTime)){
               session.status = 'C';
               session.epPastRegDeadline = true;
             }
           })
       } else if ((courseData.class.type.indexOf(classTypes.daytime) > -1) ||
-                 ((courseData.class.type.indexOf(classTypes.virtual) > -1) && (code.substr(code.lenght-1).toUpperCase=="A"))) {
+                 ((courseData.class.type.indexOf(classTypes.virtual) > -1) && (code.substr(code.length-1).toUpperCase=="A"))) {
           // for daytime courses or for virtual courses, with course code ending with 'A' are displayed
           // only the sessions that have deadline beyond the current time are displayed.
           var sessions = [];
-          courseData.session.forEach(function(session) {            
+          courseData.session.forEach(function(session) {
             var regDeadline = momentTz.tz(session.origStartDate, 'YYYY MM DD', "America/New_York");
             if (regDeadline.isAfter(momentTz().tz("America/New_York"))) {
               sessions.push(session);
