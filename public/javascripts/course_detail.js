9bf5835ed55eac85c3e799918a98f496a46ed1b7
Paul Friedman <paul@friedmanonline.net>
2016-04-12 15:28:37 -0400

    GS-1205: Refactor maps to be available from any page and add GS address to footer

--- public/javascripts/course_detail.js
+++ public/javascripts/course_detail.js
@@ -1,29 +1,3 @@
-var map, geocoder;
-var marker;
-mapApp = {
-  start: function() {
-    map = new google.maps.Map(document.getElementById('map-canvas'), {
-      zoom: 15,
-    });
-    geocoder = new google.maps.Geocoder();
-    marker = null;
-  },
-  codeAddress: function(address) {
-    geocoder.geocode({
-      'address': address
-    }, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-        });
-      } else {
-        $("#mapModalLabel").append('<div id="mapAlert" class="alert alert-danger" style="display:none;"> Sorry, unable to find address. Error: ' + status + '</div>').slideDown();
-      }
-    });
-  }
-}
-
 tableApp = {
   limitTable: function(limit) {
     $(".courseDetailSessionRow, #loadMore, #showLess").hide();
@@ -116,24 +90,5 @@ $(document).ready(function() {
   });
 
   tablemobApp.limitSessions(5);
-  $('.glyphicon-map-marker').click(function(e) {
-    e.preventDefault();
-    var cityState = $(this).data('city');
-    $("#modalSessionLocationSpan").text(cityState);
-    mapApp.start();
-    var address = $(this).data('address');
-    var destination = address.replace(/ /g, '+');
-    var directionsUrl = "http://maps.google.com?saddr=Current+Location&daddr=" + destination + "";
-    $("#map-address").html(address);
-    google.maps.event.addListenerOnce(map, 'idle', function() {
-      google.maps.event.trigger(map, 'resize');
-      if (marker != null) {
-        map.setCenter(marker.getPosition());
-      }
-    });
-
-    mapApp.codeAddress(address);
-    $("#getDirections").attr("href", directionsUrl);
-  });
   tablemobApp.initialMobileDetailExpand();
 });
