b22133b32cde94a928a5a199d07b69c6426055fc
Ifti Khan <ikhan@agiletrailblazers.com>
2016-04-13 11:31:17 -0400

    GS-1241. Contact us form uses common code.

--- public/javascripts/forms/contact_us.js
+++ public/javascripts/forms/contact_us.js
@@ -1,29 +1,3 @@
-var map, geocoder;
-var marker;
-mapApp = {
-  start: function() {
-    map = new google.maps.Map(document.getElementById('map-canvas'), {
-      zoom: 15,
-    });
-    geocoder = new google.maps.Geocoder();
-    marker = null;
-  },
-  codeAddress: function(address) {
-    geocoder.geocode({
-      'address': address
-    }, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-        });
-      } else {
-        $("#mapModalLabel").append('<div id="mapAlert" class="alert alert-danger" style="display:none;"> Sorry, unable to find address. Error: ' + status + '</div>').slideDown();
-      }
-    });
-  }
-}
-
 var contactUSFormValidate = {
   firstName: function() {
     var input = $("#txtFirstName").val();
@@ -112,26 +86,6 @@ $(document).ready(function() {
   $("#alertError").hide();
   $(".loading").hide();
   $("#removeAlert").css('cursor', 'pointer');
-
-  $('.glyphicon-map-marker').click(function(e) {
-    e.preventDefault();
-    var cityState = $(this).data('city');
-    mapApp.start();
-    var address = $(this).data('address');
-    var destination = address.replace(/ /g, '+');
-    var directionsUrl = "http://maps.google.com?saddr=Current+Location&daddr=" + destination + "";
-    $("#map-address").html(address);
-    google.maps.event.addListenerOnce(map, 'idle', function() {
-      google.maps.event.trigger(map, 'resize');
-      if (marker != null) {
-        map.setCenter(marker.getPosition());
-      }
-    });
-
-    mapApp.codeAddress(address);
-    $("#getDirections").attr("href", directionsUrl);
-  });
-
 
   $("#submitForm").click(function(e) {
     e.preventDefault();
