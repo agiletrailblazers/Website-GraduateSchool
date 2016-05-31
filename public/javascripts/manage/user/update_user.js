a0afb4f0a356978811b0486d12c3ce2d389337ef
Michael Fried <mfried@agiletrailblazers.com>
2016-05-31 11:29:42 -0400

    GS-1644:Allowing email change and show dialog

--- public/javascripts/manage/user/update_user.js
+++ public/javascripts/manage/user/update_user.js
@@ -10,7 +10,7 @@ var clearErrorAlert = function (formData) {
 $(document).ready(function () {
     $(".loading").hide();
     $("#gs-alert-remove").css('cursor', 'pointer');
-    $("#gs-alert-remove").click(function() {
+    $("#gs-alert-remove").click(function () {
         $("#gs-alert-error").slideUp();
         $("#gs-alert-error p").remove();
     });
@@ -28,54 +28,24 @@ $(document).ready(function () {
 
     $("#update-user-form-submit").click(function (event) {
         event.preventDefault();
-        $(".loading").show();
-        var formData = {
-            firstName: $("#firstName").val(),
-            middleName: $("#middleName").val(),
-            lastName: $("#lastName").val(),
-            dateOfBirth: $("#dateOfBirth").val(),
-            lastFourSSN: $("#lastFourSSN").val(),
-            street: $("#street").val(),
-            suite: $("#suite").val(),
-            timezoneId: $("#timezone").val(),
-            city: $("#city").val(),
-            state: $("#state").val(),
-            zip: $("#zip").val(),
-            email: $("#email").val(),
-            phone: $("#phone").val()
-        };
 
-        clearErrorAlert(formData);
+        var oldEmail = $("#oldEmail").val().toLowerCase();
+        var newEmail = $("#email").val().toLowerCase();
 
-        if (!$("#gs-alert-error p").length) {
-            $.post("/manage/user/update_user", formData)
-                .done(function () {
-                    // reload the page to reflect new user info
-                    window.location.reload();
-
-                })
-                .fail(function (xhr, textStatus, errorThrown) {
-                    $(".loading").hide();
-                    alertify.error("Error updating user.");
-                    var errors = xhr.responseJSON;
-                    for (var key in errors) {
-                        if (key === "validationErrors") {
-                            var validationErrors = errors[key];
-                            for (var i = 0; i < validationErrors.length; i++) {
-                                if (validationErrors[i].fieldName === "person.emailAddress") continue;
-                                $("#gs-alert-error").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> " + errorMessageForFieldName(validationErrors[i].fieldName) + "</p>");
-                            }
-                        }
-                        else if (errors.hasOwnProperty(key)) {
-                            $("#gs-alert-error").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> " + errors[key] + "</p>");
-                        }
-                    }
-                    $("#gs-alert-error").slideDown();
-                    $("html, body").animate({
-                        scrollTop: 220
-                    }, "slow");
-                });
+        if (newEmail && oldEmail && (oldEmail != newEmail)){
+            $('#usernameConfirmModal').modal('show');
+        } else {
+            submitUpdateForm();
         }
+    });
+
+    $('#usernameChangeConfirm').click(function (e) {
+        $('#usernameConfirmModal').modal('hide');
+        submitUpdateForm();
+    });
+
+    $('#usernameChangeCancel').click(function (e) {
+        $('#usernameConfirmModal').modal('hide');
     });
 
 });
@@ -122,5 +92,57 @@ function errorMessageForFieldName(fieldName) {
             return "Zip is required and must be at least 5 characters.";
         default:
             return fieldName;
+    }
+}
+
+function submitUpdateForm() {
+    window.location.href='#container';
+    $(".loading").show();
+    var formData = {
+        firstName: $("#firstName").val(),
+        middleName: $("#middleName").val(),
+        lastName: $("#lastName").val(),
+        dateOfBirth: $("#dateOfBirth").val(),
+        lastFourSSN: $("#lastFourSSN").val(),
+        street: $("#street").val(),
+        suite: $("#suite").val(),
+        timezoneId: $("#timezone").val(),
+        city: $("#city").val(),
+        state: $("#state").val(),
+        zip: $("#zip").val(),
+        email: $("#email").val(),
+        phone: $("#phone").val()
+    };
+
+    clearErrorAlert(formData);
+
+    if (!$("#gs-alert-error p").length) {
+        $.post("/manage/user/update_user", formData)
+            .done(function () {
+                // reload the page to reflect new user info
+                window.location.reload();
+
+            })
+            .fail(function (xhr, textStatus, errorThrown) {
+                $(".loading").hide();
+                alertify.error("Error updating user.");
+                var errors = xhr.responseJSON;
+                for (var key in errors) {
+                    if (key === "validationErrors") {
+                        var validationErrors = errors[key];
+                        for (var i = 0; i < validationErrors.length; i++) {
+                            if (validationErrors[i].fieldName === "person.emailAddress") continue;
+                            $("#gs-alert-error").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> " + errorMessageForFieldName(validationErrors[i].fieldName) + "</p>");
+                        }
+                    }
+                    else if (errors.hasOwnProperty(key)) {
+                        $("#gs-alert-error").append("<p><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> " + errors[key] + "</p>");
+                    }
+                }
+                $("#gs-alert-error").slideDown();
+                $("html, body").animate({
+                    scrollTop: 220
+                }, "slow");
+            });
     }
 }
