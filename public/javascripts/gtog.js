efd9deb4c2b6523496ba1128ca9642ac2e8e637b
Ifti Khan <ikhan@agiletrailblazers.com>
2016-06-03 12:46:40 -0400

    GS-1677. Once a column was selected to sort in ascending order, we were not able to sort it by descending order. This is fixed.

--- public/javascripts/gtog.js
+++ public/javascripts/gtog.js
@@ -9,10 +9,8 @@ $(document).ready(function() {
     $(this).css("height",$height)
   });
 
-  $("#gtog0, #gtog1, #gtog2, #gtog3, #gtog4, #gtog5, #gtog6").tablesorter();
-  $("table").tablesorter({
+  $("#gtog0, #gtog1, #gtog2, #gtog3, #gtog4, #gtog5, #gtog6").tablesorter({
     // sort on the third column ie start date, and then on 2nd ie course code order asc
     sortList: [[2,0],[1,0]]
   });
-
 });
